0398efed156674f60896c2d78ef60dca9530649d
이석진 <seokjin.dev@gmail.com>
2019-12-23 10:09:52 +0900

    game 클래스 설계

---
+++ include/Game.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+class game
+{
+public:
+	game();
+	bool initialize();
+	void run_loop();
+	void shutdown();
+
+private:
+	void process_input();
+	void update_game();
+	void generate_input();
+
+	class SDL_Window* window_;
+	bool is_running_;
+};
