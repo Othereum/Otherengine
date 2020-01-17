8f14c1e6ca797486759d579a19815fcc80b24e8d
Othereum <seokjin.dev@gmail.com>
2020-01-17 16:24:25 +0900

    수학 함수 추가

--- include/MathUtils.h
+++ include/MathUtils.h
@@ -17,6 +17,12 @@ constexpr T max(T a, T b) { return a > b ? a : b; }
 template <class T>
 constexpr T clamp(T v, T min, T max) { return math::max(math::min(v, max), min); }
 
+template <class T>
+constexpr T abs(T v) { return v < 0 ? -v : v; }
+
+template <class T>
+constexpr bool is_nearly_equal(T a, T b, T tolerance) { return abs(a-b) <= tolerance; }
+
 MATH_END
 NEG_END
 
