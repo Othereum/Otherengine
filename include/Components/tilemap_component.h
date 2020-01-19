3313869ade09bf8bd64b6466339cf84af065332e
Othereum <seokjin.dev@gmail.com>
2020-01-19 17:05:45 +0900

    tilemap_component 선언

---
+++ include/Components/tilemap_component.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "sprite_component.h"
+#include <vector>
+
+namespace game
+{
+	class tilemap_component : public sprite_component
+	{
+	public:
+		explicit tilemap_component(actor& owner, int draw_order = 10, int update_order = 100);
+		void load_tile_from_csv(const char* csv_filename);
+		void draw(renderer& renderer) const override;
+
+	private:
+		std::vector<std::vector<int>> tile_;
+	};
+}
