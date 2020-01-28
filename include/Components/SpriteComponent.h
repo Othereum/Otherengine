e2aecc8381484b90c3a1cd45a42553712f7f904c
Othereum <seokjin.dev@gmail.com>
2020-01-28 19:38:54 +0900

    함수 명시적 삭제

--- include/Components/SpriteComponent.h
+++ include/Components/SpriteComponent.h
@@ -12,6 +12,12 @@ class sprite_component : public component
 {
 public:
 	explicit sprite_component(actor& owner, int draw_order = 100, int update_order = 100);
+	~sprite_component();
+
+	sprite_component(const sprite_component&) = delete;
+	sprite_component(sprite_component&&) = delete;
+	sprite_component& operator=(const sprite_component&) = delete;
+	sprite_component& operator=(sprite_component&&) = delete;
 
 	virtual void draw(SDL_Renderer& renderer) const;
 	virtual void set_texture(std::shared_ptr<SDL_Texture>&& texture);
