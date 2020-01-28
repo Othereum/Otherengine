75d93c8affc73e9e3f4bc45f50fce1f355a3032c
Othereum <seokjin.dev@gmail.com>
2020-01-28 18:51:35 +0900

    sprite_component 구현

---
+++ include/Components/SpriteComponent.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Components/Component.h>
+#include <memory>
+
+class SDL_Renderer;
+class SDL_Texture;
+
+NEG_BEGIN
+
+class sprite_component : public component
+{
+public:
+	explicit sprite_component(actor& owner, int draw_order = 100, int update_order = 100);
+
+	virtual void draw(SDL_Renderer& renderer);
+	virtual void set_texture(std::shared_ptr<SDL_Texture>&& texture);
+
+	[[nodiscard]] unsigned short get_tex_w() const { return tex_w_; }
+	[[nodiscard]] unsigned short get_tex_h() const { return tex_h_; }
+	[[nodiscard]] int get_draw_order() const { return draw_order_; }
+
+private:
+	unsigned short tex_w_{}, tex_h_{};
+	int draw_order_;
+	std::shared_ptr<SDL_Texture> texture_;
+};
+
+NEG_END
---
+++ src/Components/SpriteComponent.cpp
@@ -0,0 +1,25 @@
+#include <Components/SpriteComponent.h>
+#include <SDL.h>
+
+NEG_BEGIN
+
+sprite_component::sprite_component(actor& owner, const int draw_order, const int update_order)
+	:component{owner, update_order}, draw_order_{draw_order}
+{
+}
+
+void sprite_component::draw(SDL_Renderer& renderer)
+{
+}
+
+void sprite_component::set_texture(std::shared_ptr<SDL_Texture>&& texture)
+{
+	texture_ = std::move(texture);
+
+	int w, h;
+	SDL_QueryTexture(texture_.get(), nullptr, nullptr, &w, &h);
+	tex_w_ = w;
+	tex_h_ = h;
+}
+
+NEG_END
