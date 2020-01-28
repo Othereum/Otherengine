2ef263eb513c5c076bdc5f6508333969a36bc75d
Othereum <seokjin.dev@gmail.com>
2020-01-28 11:43:41 +0900

    static

--- src/Game.cpp
+++ src/Game.cpp
@@ -12,14 +12,14 @@ NEG_BEGIN
 static constexpr auto screen_w = 1024;
 static constexpr auto screen_h = 768;
 
-SDL_Window* create_window()
+static SDL_Window* create_window()
 {	
 	const auto window = SDL_CreateWindow("No Engine Game", 100, 100, screen_w, screen_h, 0);
 	if (!window) throw sdl_error{join("Failed to create window: ", SDL_GetError())};
 	return window;
 }
 
-SDL_Renderer* create_renderer(SDL_Window* const window)
+static SDL_Renderer* create_renderer(SDL_Window* const window)
 {
 	const auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (!renderer) throw sdl_error{join("Failed to create renderer: ", SDL_GetError())};
