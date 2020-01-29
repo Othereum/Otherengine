6aeb7d1a3c185204ed37b87cf64686019219c1ae
Othereum <seokjin.dev@gmail.com>
2020-01-29 12:36:29 +0900

    template 개선

--- include/MathUtils.h
+++ include/MathUtils.h
@@ -8,20 +8,20 @@
 NEG_BEGIN
 MATH_BEGIN
 
-template <class T>
-constexpr T min(T a, T b) { return a < b ? a : b; }
+template <class T, class U>
+constexpr auto min(T a, U b) { return a < b ? a : b; }
 
-template <class T>
-constexpr T max(T a, T b) { return a > b ? a : b; }
+template <class T, class U>
+constexpr auto max(T a, U b) { return a > b ? a : b; }
 
-template <class T>
-constexpr T clamp(T v, T min, T max) { return math::max(math::min(v, max), min); }
+template <class T, class U, class V>
+constexpr auto clamp(T v, U min, V max) { return math::max(math::min(v, max), min); }
 
 template <class T>
 constexpr T abs(T v) { return v < 0 ? -v : v; }
 
-template <class T>
-constexpr bool is_nearly_equal(T a, T b, T tolerance) { return abs(a-b) <= tolerance; }
+template <class T, class U, class V>
+constexpr bool is_nearly_equal(T a, U b, V tolerance) { return abs(a-b) <= tolerance; }
 
 MATH_END
 NEG_END
