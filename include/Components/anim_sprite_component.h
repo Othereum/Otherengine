1aac17238d1a2517b103401660446244c9c5cba0
Othereum <seokjin.dev@gmail.com>
2020-02-01 14:05:31 +0900

    일부 함수 구현

--- include/Components/anim_sprite_component.h
+++ include/Components/anim_sprite_component.h
@@ -7,12 +7,18 @@ namespace game
 	class anim_sprite_component : public sprite_component
 	{
 	public:
-		explicit anim_sprite_component(actor& owner, int draw_order = 100, int update_order = 100);
+		explicit anim_sprite_component(actor& owner, int draw_order = 100, int update_order = 100)
+			:sprite_component{owner, draw_order, update_order}
+		{
+		}
 		
-		void set_anim_textures(std::vector<std::shared_ptr<SDL_Texture>>&& textures);
+		void set_anim_textures(std::vector<std::shared_ptr<SDL_Texture>>&& textures)
+		{
+			anim_textures_ = std::move(textures);
+		}
 		
-		void set_anim_fps(float fps);
-		[[nodiscard]] float get_anim_fps() const;
+		void set_anim_fps(float fps) { anim_fps_ = fps; }
+		[[nodiscard]] float get_anim_fps() const { return anim_fps_; }
 
 	private:
 		void update(float delta_seconds) override;
