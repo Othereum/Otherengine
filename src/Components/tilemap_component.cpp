5136a33b05ed00b5f2c87589389d7bf60ead86f8
Othereum <seokjin.dev@gmail.com>
2020-02-04 17:52:43 +0900

    Implement load_tile_from_csv

--- src/Components/tilemap_component.cpp
+++ src/Components/tilemap_component.cpp
@@ -1,9 +1,28 @@
 #include "components/tilemap_component.h"
+#include <fstream>
+#include <sstream>
+#include <string>
 
 namespace game
 {
 	tilemap_component::tilemap_component(actor& owner, int draw_order, int update_order)
 		:sprite_component{owner, draw_order, update_order}
 	{
+	}
+
+	void tilemap_component::load_tile_from_csv(const char* csv_filename)
+	{
+		tile_.clear();
+		std::ifstream csv{csv_filename};
+		for (std::string line; std::getline(csv, line);)
+		{
+			std::vector<int> row;
+			std::stringstream ss{line};
+			for (std::string cell; std::getline(ss, cell, ',');)
+			{
+				row.push_back(std::stoi(cell));
+			}
+			tile_.push_back(std::move(row));
+		}
 	}
 }
