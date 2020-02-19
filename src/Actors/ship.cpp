c5075002f9dff5260813139fb0fee7c0d7cb3efa
Othereum <seokjin.dev@gmail.com>
2020-02-19 10:28:13 +0900

    ship 구현부

---
+++ src/Actors/ship.cpp
@@ -0,0 +1,17 @@
+#include "actors/ship.h"
+
+namespace game
+{
+	ship::ship(world& outer)
+		:actor{outer}
+	{
+	}
+
+	void ship::update_actor(float delta_seconds)
+	{
+	}
+
+	void ship::process_keyboard(const uint8_t* state)
+	{
+	}
+}
