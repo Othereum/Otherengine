f2d7a6990bb5b4e4d33a1644a7c0ff76ba49b61a
Othereum <seokjin.dev@gmail.com>
2020-02-20 12:19:41 +0900

    ship 구현

--- src/Actors/ship.cpp
+++ src/Actors/ship.cpp
@@ -1,17 +1,44 @@
 #include "actors/ship.h"
+#include <SDL.h>
+#include "components/anim_sprite_component.h"
+#include "world.h"
+#include "application.h"
+#include "renderer.h"
 
 namespace game
 {
 	ship::ship(world& outer)
 		:actor{outer}
 	{
+		auto& renderer = outer.get_app().get_renderer();
+		auto& anim = add_component<anim_sprite_component>();
+		anim.set_anim_textures({
+			renderer.get_texture("Assets/Ship01.png"),
+			renderer.get_texture("Assets/Ship02.png"),
+			renderer.get_texture("Assets/Ship03.png"),
+			renderer.get_texture("Assets/Ship04.png")
+		});
 	}
 
-	void ship::update_actor(float delta_seconds)
+	void ship::update_actor(const float delta_seconds)
 	{
+		set_pos(get_pos() + velocity_ * delta_seconds);
 	}
 
 	void ship::process_keyboard(const uint8_t* state)
 	{
+		velocity_ = {};
+		
+		if (state[SDL_SCANCODE_W])
+			velocity_.y += 100;
+
+		if (state[SDL_SCANCODE_S])
+			velocity_.y -= 100;
+
+		if (state[SDL_SCANCODE_A])
+			velocity_.x -= 100;
+
+		if (state[SDL_SCANCODE_D])
+			velocity_.x += 100;
 	}
 }
