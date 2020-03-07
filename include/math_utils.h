df2d3dfd7763337fad0a2669a7c917131362b5e8
Othereum <seokjin.dev@gmail.com>
2020-03-08 00:04:27 +0900

    Add small and kinda_small

--- include/math_utils.h
+++ include/math_utils.h
@@ -5,6 +5,8 @@ namespace game::math
 {
 	using pi_ratio = std::ratio<5419351, 1725033>;
 	constexpr auto pi = static_cast<float>(pi_ratio::num) / pi_ratio::den;
+	constexpr auto small = 1e-8f;
+	constexpr auto kinda_small = 1e-4f;
 
 	template <class T, class U>
 	constexpr auto min(T a, U b) noexcept { return a < b ? a : b; }
