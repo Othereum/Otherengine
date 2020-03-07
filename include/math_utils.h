7b7dc3edd705840517d0dff15b0e55dc094b3989
Othereum <seokjin.dev@gmail.com>
2020-03-08 00:05:13 +0900

    Add is_nearly_zero()

--- include/math_utils.h
+++ include/math_utils.h
@@ -17,9 +17,9 @@ namespace game::math
 	template <class T, class U, class V>
 	constexpr auto clamp(T v, U min, V max) noexcept { return math::max(math::min(v, max), min); }
 
-	template <class T>
-	constexpr T abs(T v) { return v < 0 ? -v : v; }
+	template <class T, class U, class V = float>
+	bool is_nearly_equal(T a, U b, V tolerance = small) noexcept { return abs(a-b) < tolerance; }
 
-	template <class T, class U, class V>
-	constexpr bool is_nearly_equal(T a, U b, V tolerance) { return abs(a-b) <= tolerance; }
+	template <class T, class U = float>
+	bool is_nearly_zero(T a, U tolerance = small) noexcept { return is_nearly_equal(a, 0, tolerance); }
 }
