5c7291b25d47ea041c9cd998819203142ba33a91
Othereum <seokjin.dev@gmail.com>
2020-03-10 18:13:57 +0900

    Add bind functions

--- include/Components/input_component.h
+++ include/Components/input_component.h
@@ -1,11 +1,44 @@
 #pragma once
 #include "component.h"
+#include <map>
+#include <functional>
 
 namespace game
 {
+	enum class key_event { released, pressed };
+	using key_t = int;
+
+	struct input_action
+	{
+		[[nodiscard]] virtual std::pair<const key_t*, size_t> keys() const noexcept = 0;
+	};
+
+	struct axis_t
+	{
+		key_t key;
+		float scale;
+	};
+	
+	struct input_axis
+	{
+		[[nodiscard]] virtual std::pair<const axis_t*, size_t> keys() const noexcept = 0;
+	};
+
 	class input_component : public component
 	{
 	public:
+		explicit input_component(class actor& owner, int update_order = 1);
 		
+		void bind_action(const input_action& action, key_event event, std::function<void()>&& callback);
+		void bind_axis(const input_axis& axis, std::function<void()>&& callback);
+		
+	private:
+		using fn_idx_t = size_t;
+		
+		std::multimap<key_t, fn_idx_t> actions_[2];
+		std::vector<std::function<void()>> action_fns_;
+		
+		std::vector<std::pair<axis_t, fn_idx_t>> axises_;
+		std::vector<std::function<void(float)>> axis_fns_;
 	};
 }
---
+++ src/Components/input_component.cpp
@@ -0,0 +1,28 @@
+#include "components/input_component.h"
+
+namespace game
+{
+	void input_component::bind_action(const input_action& action, key_event event, std::function<void()>&& callback)
+	{
+		const auto idx = action_fns_.size();
+		action_fns_.emplace_back(std::move(callback));
+		
+		const auto [keys, size] = action.keys();
+		for (size_t i = 0; i < size; ++i)
+		{
+			actions_[int(event)].emplace(keys[i], idx);
+		}
+	}
+
+	void input_component::bind_axis(const input_axis& axis, std::function<void()>&& callback)
+	{
+		const auto idx = axis_fns_.size();
+		axis_fns_.emplace_back(std::move(callback));
+		
+		const auto [axises, size] = axis.keys();
+		for (size_t i = 0; i < size; ++i)
+		{
+			axises_.emplace_back(axises[i], idx);
+		}
+	}
+}
