2ef77333ba63ff5b3ceaeda866142b4e2526370e
Othereum <seokjin.dev@gmail.com>
2020-03-11 21:17:27 +0900

    Add: move component declaration

---
+++ include/Components/move_component.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "component.h"
+#include "vector.h"
+
+namespace game
+{
+	class move_component : public component
+	{
+	public:
+		explicit move_component(actor& owner, int update_order = 10);
+
+		void update(float delta_seconds) override;
+		
+		void add_movement_input(fvector2 v);
+		void add_rotation_input(float r);
+
+	private:
+		fvector2 movement_;
+		float rotation_ = 0;
+	};
+}
