fd6106ecce2699fb8f26ab1f91bda32ba1ebfcb2
Othereum <seokjin.dev@gmail.com>
2020-03-12 19:59:13 +0900

    Implement: asteroid

---
+++ src/Actors/asteroid.cpp
@@ -0,0 +1,17 @@
+#include "actors/asteroid.h"
+#include "components/move_component.h"
+#include "components/sprite_component.h"
+#include "math_utils.h"
+
+namespace game
+{
+	asteroid::asteroid(world& outer)
+		:actor{outer}
+	{
+		auto& sprite = add_component<sprite_component>();
+		sprite.set_texture("Assets/Asteroid.png");
+
+		auto& movement = add_component<move_component>();
+		movement.set_velocity(math::rand_unit_vec() * 150);
+	}
+}
