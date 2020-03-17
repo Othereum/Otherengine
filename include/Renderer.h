1b01e7c573899038db3b0dc8093a62a7786aa3f4
Othereum <seokjin.dev@gmail.com>
2020-03-17 21:56:30 +0900

    feat: Add CRenderer class

---
+++ include/Renderer.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <memory>
+#include "Rotation.h"
+#include "Vector.h"
+
+struct SDL_Texture;
+struct SDL_Rect;
+struct SDL_Renderer;
+struct SDL_Window;
+
+namespace Game
+{
+	struct TFRect;
+	class CSpriteComponent;
+	
+	constexpr FVector2<uint16_t> kScrSz{1024, 768};
+	
+	class CRenderer
+	{
+	public:
+		void RegisterSprite(const CSpriteComponent& sprite);
+		void UnregisterSprite(const CSpriteComponent& sprite);
+
+		void Draw(SDL_Texture& texture, const TFRect& dest, TDegrees angle = {}) const;
+		void Draw(SDL_Texture& texture, const SDL_Rect& src, const SDL_Rect& dest, TDegrees angle = {}) const;
+
+	private:
+		using TWindowPtr = std::unique_ptr<SDL_Window, void(*)(SDL_Window*)>;
+		using TRendererPtr = std::unique_ptr<SDL_Renderer, void(*)(SDL_Renderer*)>;
+	
+		TWindowPtr window_;
+		TRendererPtr renderer_;
+
+		std::vector<std::reference_wrapper<const CSpriteComponent>> sprites_;
+	};
+}
