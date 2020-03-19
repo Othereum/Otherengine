dc5ae582b96e0b00935f8e8d5e98ac34dece2178
Othereum <seokjin.dev@gmail.com>
2020-03-20 03:54:05 +0900

    refactor: Remove codes

--- include/Engine.h
+++ include/Engine.h
@@ -1,9 +1,8 @@
 ﻿#pragma once
 
-#include <chrono>
 #include <memory>
 #include <unordered_map>
-#include "Rotation.h"
+#include "Name.h"
 
 struct SDL_Window;
 struct SDL_Texture;
@@ -12,11 +11,8 @@ struct SDL_Rect;
 
 namespace Game
 {
-	class CSpriteComponent;
-	class CRenderer;
-	class AActor;
-	class CInputComponent;
-	class CCircleComponent;
+	class CWorld;
+	class CInputSystem;
 
 	class CSdlRaii
 	{
@@ -33,51 +29,19 @@ namespace Game
 		
 		void RunLoop();
 		void Shutdown();
-
-		template <class T>
-		T& SpawnActor()
-		{
-			static_assert(std::is_base_of_v<AActor, T>);
-			auto ptr = std::make_unique<T>(*this);
-			auto& actor = *ptr;
-			RegisterActor(std::move(ptr));
-			return actor;
-		}
 		
-		void RegisterInputComponent(const CInputComponent& comp);
-		void UnregisterInputComponent(const CInputComponent& comp);
-
-		void RegisterCollision(CCircleComponent& comp);
-		void UnregisterCollision(CCircleComponent& comp);
-		
-		[[nodiscard]] std::shared_ptr<SDL_Texture> GetTexture(const char* filename);
-		[[nodiscard]] auto GetTime() const noexcept { return time_; }
-		[[nodiscard]] CRenderer& GetRenderer() const noexcept { return *renderer_; }
+		[[nodiscard]] std::shared_ptr<SDL_Texture> GetTexture(FName file);
+		[[nodiscard]] CWorld& GetWorld() const noexcept { return *world_; }
+		[[nodiscard]] const CInputSystem& GetInputSystem() const noexcept { return *inputSystem_; }
 		
 	private:
-		void ProcessInput();
-		void UpdateGame();
-		void GenerateOutput() const;
-		
-		float UpdateTime();
-		
-		void RegisterActor(std::unique_ptr<AActor>&& actor);
-		
-		[[nodiscard]] std::shared_ptr<SDL_Texture> LoadTexture(const char* filename);
+		void Tick();
+		void ProcessEvent();
+		[[nodiscard]] std::shared_ptr<SDL_Texture> LoadTexture(FName file);
 
-
-		std::unique_ptr<CRenderer> renderer_;
-
-		std::unordered_map<std::string, std::weak_ptr<SDL_Texture>> textures_;
-		
-		std::vector<std::reference_wrapper<const CInputComponent>> inputComps_;
-		std::vector<std::reference_wrapper<CCircleComponent>> collisions_;
-		
-		std::vector<std::unique_ptr<AActor>> actors_;
-		std::vector<std::unique_ptr<AActor>> pendingActors_;
-		
-		std::chrono::steady_clock::time_point time_;
-
+		std::unordered_map<FName, std::weak_ptr<SDL_Texture>> textures_;
+		std::unique_ptr<CWorld> world_;
+		std::unique_ptr<CInputSystem> inputSystem_;
 		bool bIsRunning_ = true;
 	};
 }
--- src/Engine.cpp
+++ src/Engine.cpp
@@ -4,10 +4,11 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
-#include "Renderer.h"
-#include "Actor/Actor.h"
 #include "Component/InputComponent.h"
 #include "Component/CircleComponent.h"
+#include "InputSystem.h"
+#include "Renderer.h"
+#include "World.h"
 
 #include "Actor/Asteroid.h"
 #include "Actor/Ship.h"
@@ -16,17 +17,31 @@
 namespace Game
 {
 	CEngine::CEngine() :
-		renderer_{std::make_unique<CRenderer>()},
-		time_{std::chrono::steady_clock::now()}
+		world_{std::make_unique<CWorld>(*this)},
+		inputSystem_{std::make_unique<CInputSystem>()}
 	{
+		inputSystem_->AddAxis("MoveForward", {
+			{'w', EInputType::keyboard, 1},
+			{'s', EInputType::keyboard, -1},
+		});
+		
+		inputSystem_->AddAxis("Turn", {
+			{'a', EInputType::keyboard, -1},
+			{'d', EInputType::keyboard, 1},
+		});
+
+		inputSystem_->AddAction("Shoot", {
+			{' ', EInputType::keyboard}
+		});
+		
 		for (auto i = 0; i < 20; ++i)
 		{
-			auto& ast = SpawnActor<AAsteroid>();
+			auto& ast = world_->SpawnActor<AAsteroid>();
 			ast.SetPos(Math::RandVec({0, 0}, TFVector2{kScrSz}));
 			ast.SetRot(Math::RandAng());
 		}
 
-		auto& sh = SpawnActor<ship>();
+		auto& sh = world_->SpawnActor<ship>();
 		sh.SetPos(TFVector2{kScrSz / 2});
 	}
 
@@ -36,9 +51,7 @@ namespace Game
 	{
 		while (bIsRunning_)
 		{
-			ProcessInput();
-			UpdateGame();
-			GenerateOutput();
+			Tick();
 		}
 	}
 
@@ -47,147 +60,64 @@ namespace Game
 		bIsRunning_ = false;
 	}
 
-	void CEngine::RegisterInputComponent(const CInputComponent& comp)
+	std::shared_ptr<SDL_Texture> CEngine::GetTexture(FName file)
 	{
-		auto cmp = [](const CInputComponent& a, const CInputComponent& b)
-		{
-			return a.GetReceiveOrder() <= b.GetReceiveOrder();
-		};
-		const auto pos = std::lower_bound(inputComps_.begin(), inputComps_.end(), comp, cmp);
-		inputComps_.emplace(pos, comp);
-	}
-
-	void CEngine::UnregisterInputComponent(const CInputComponent& comp)
-	{
-		auto pr = [&](const CInputComponent& v) { return &v == &comp; };
-		const auto found = std::find_if(inputComps_.crbegin(), inputComps_.crend(), pr);
-		if (found != inputComps_.crend()) inputComps_.erase(found.base() - 1);
-	}
-
-	void CEngine::RegisterCollision(CCircleComponent& comp)
-	{
-		collisions_.emplace_back(comp);
-	}
-
-	void CEngine::UnregisterCollision(CCircleComponent& comp)
-	{
-		auto pr = [&](const CCircleComponent& v) { return &v == &comp; };
-		const auto found = std::find_if(collisions_.crbegin(), collisions_.crend(), pr);
-		if (found != collisions_.crend()) collisions_.erase(found.base() - 1);
-	}
-
-	std::shared_ptr<SDL_Texture> CEngine::GetTexture(const char* filename)
-	{
-		const auto found = textures_.find(filename);
+		const auto found = textures_.find(file);
 		if (found != textures_.end()) return found->second.lock();
 
-		const auto loaded = LoadTexture(filename);
-		textures_.emplace(filename, loaded);
+		const auto loaded = LoadTexture(file);
+		textures_.emplace(file, loaded);
 
 		return loaded;
 	}
 
-	void CEngine::ProcessInput()
+	void CEngine::Tick()
 	{
-		std::vector<SDL_Keycode> keyEvents[2];
+		ProcessEvent();
+		world_->Tick();
+	}
+
+	void CEngine::ProcessEvent()
+	{
+		inputSystem_->ClearEvents();
 		
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
 		{
-			switch (event.type)
+			if (event.type == SDL_QUIT)
 			{
-			case SDL_QUIT:
 				Shutdown();
 				return;
-
-			case SDL_KEYDOWN: case SDL_KEYUP:
-				if (!event.key.repeat) keyEvents[event.key.state].push_back(event.key.keysym.sym);
-				break;
-				
-			default: ;
 			}
-		}
-
-		const auto keyboard = SDL_GetKeyboardState(nullptr);
-		for (auto comp : inputComps_)
-		{
-			comp.get().ProcessInput(keyEvents, keyboard);
+			
+			inputSystem_->AddEvent(event);
 		}
 	}
 
-	void CEngine::UpdateGame()
+	std::shared_ptr<SDL_Texture> CEngine::LoadTexture(FName file)
 	{
-		const auto deltaSeconds = UpdateTime();
-		for (const auto& actor : actors_)
-			actor->Update(deltaSeconds);
-
-		
-		for (size_t i = 0; i < collisions_.size(); ++i)
-			for (auto j = i+1; j < collisions_.size(); ++j)
-				collisions_[i].get().TestOverlap(collisions_[j]);
-
-		
-		for (auto&& pending : pendingActors_)
-		{
-			pending->BeginPlay();
-			actors_.push_back(std::move(pending));
-		}
-		pendingActors_.clear();
-
-		
-		for (auto it = actors_.rbegin(); it != actors_.rend();)
-		{
-			const auto& actor = **it;
-			if (actor.GetState() == AActor::EState::dead)
-			{
-				const auto next = actors_.erase(it.base() - 1);
-				it = std::make_reverse_iterator(next);
-			}
-			else
-			{
-				++it;
-			}
-		}
-	}
-
-	void CEngine::GenerateOutput() const
-	{
-		renderer_->DrawScene();
-	}
-
-	float CEngine::UpdateTime()
-	{
-		using namespace std::chrono;
-		const auto now = steady_clock::now();
-		const auto deltaSeconds = duration<float>{now - time_}.count();
-		time_ = now;
-		return deltaSeconds;
-	}
-
-	void CEngine::RegisterActor(std::unique_ptr<AActor>&& actor)
-	{
-		pendingActors_.push_back(std::move(actor));
-	}
-
-	std::shared_ptr<SDL_Texture> CEngine::LoadTexture(const char* filename)
-	{
-		const std::unique_ptr<SDL_Surface, void(*)(SDL_Surface*)> surface{IMG_Load(filename), SDL_FreeSurface};
+		const std::unique_ptr<SDL_Surface, void(*)(SDL_Surface*)> surface{
+			IMG_Load(file.Str().c_str()),
+			SDL_FreeSurface
+		};
 		if (!surface) throw std::runtime_error{SDL_GetError()};
 
-		auto deleter = [this, filename](SDL_Texture* const texture)
-		{
-			textures_.erase(filename);
-			SDL_DestroyTexture(texture);
+		std::shared_ptr<SDL_Texture> texture{
+			SDL_CreateTextureFromSurface(&world_->GetRenderer().GetSdlRenderer(), surface.get()),
+			[this, file](SDL_Texture* texture)
+			{
+				textures_.erase(file);
+				SDL_DestroyTexture(texture);
+			}
 		};
-		std::shared_ptr<SDL_Texture> texture{SDL_CreateTextureFromSurface(&renderer_->GetSdlRenderer(), surface.get()), std::move(deleter)};
 		if (!texture) throw std::runtime_error{SDL_GetError()};
-
+		
 		return texture;
 	}
 
 	CSdlRaii::CSdlRaii()
 	{
-		const auto sdlResult = SDL_Init(SDL_INIT_VIDEO);
+		const auto sdlResult = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER);
 		if (sdlResult != 0) throw std::runtime_error{SDL_GetError()};
 
 		const auto flags = IMG_INIT_PNG;
