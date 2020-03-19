7cad0b32b48606abc075ed33c6974426c2a49a15
Othereum <seokjin.dev@gmail.com>
2020-03-20 03:50:19 +0900

    refactor: InputSystem

--- include/InputSystem.h
+++ include/InputSystem.h
@@ -1,35 +1,68 @@
 #pragma once
 #include <vector>
+#include <unordered_map>
+#include "Name.h"
 
 union SDL_Event;
 
 namespace Game
-{
-	enum class EInputType : uint8_t
+{	
+	enum class EInputType : char
 	{
-		keyboard, mouse, controller
+		invalid = -1, keyboard, mButton, mAxisX, mAxisY, cButton, cAxis
 	};
 
-	struct FKeyData
+	struct FInputKey
 	{
-		int key;
-		uint16_t mod;
-		EInputType type;
+		int key = -1;
+		EInputType type = EInputType::invalid;
+		
+		constexpr FInputKey() noexcept = default;
+		constexpr FInputKey(int key, EInputType type) noexcept: key{key}, type{type} {}
+		
+		bool operator==(const FInputKey&) const noexcept = default;
+	};
+
+	struct FInputAxis : FInputKey
+	{
+		float scale = 1;
+		
+		constexpr FInputAxis() noexcept = default;
+		constexpr FInputAxis(int key, EInputType type, float scale) noexcept: FInputKey{key, type}, scale{scale} {}
+	};
+
+	struct FInputAction : FInputKey
+	{
+		uint16_t mod = 0;
+
+		constexpr FInputAction() noexcept = default;
+		constexpr FInputAction(int key, EInputType type, uint16_t mod = 0) noexcept: FInputKey{key, type}, mod{mod} {}
 	};
 
 	struct FInputEvent
 	{
-		FKeyData key;
+		FName name;
 		bool bPressed;
 	};
-	
+
 	class CInputSystem
 	{
 	public:
 		void AddEvent(const SDL_Event& e);
 		void ClearEvents();
 
+		void AddAxis(FName name, std::vector<FInputAxis>&& keys);
+		void AddAction(FName name, std::vector<FInputAction>&& keys);
+		
+		[[nodiscard]] float GetAxisValue(FName name) const noexcept;
+		[[nodiscard]] static float GetAxisValue(const FInputAxis& axis) noexcept;
+		[[nodiscard]] auto& GetEvents() const noexcept { return events_; }
+		[[nodiscard]] auto& GetAxises() const noexcept { return axises_; }
+		[[nodiscard]] auto& GetActions() const noexcept { return actions_; }
+
 	private:
 		std::vector<FInputEvent> events_;
+		std::unordered_map<FName, std::vector<FInputAxis>> axises_;
+		std::unordered_map<FName, std::vector<FInputAction>> actions_;
 	};
 }
