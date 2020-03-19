2d2797455ef1a324cccdb56fd16fe117371170c4
Othereum <seokjin.dev@gmail.com>
2020-03-20 03:52:52 +0900

    feat: Implement CWorld

---
+++ src/World.cpp
@@ -0,0 +1,81 @@
+#include "World.h"
+
+#include "Actor/Actor.h"
+#include "Component/CircleComponent.h"
+#include "Component/InputComponent.h"
+#include "Renderer.h"
+
+namespace Game
+{
+	CWorld::CWorld(CEngine& engine):
+		engine_{engine},
+		renderer_{std::make_unique<CRenderer>()}
+	{
+	}
+
+	void CWorld::Tick()
+	{
+		UpdateGame();
+		renderer_->DrawScene();
+	}
+
+	void CWorld::RegisterCollision(CCircleComponent& comp)
+	{
+		collisions_.emplace_back(comp);
+	}
+
+	void CWorld::UnregisterCollision(CCircleComponent& comp)
+	{
+		auto pr = [&](const CCircleComponent& v) { return &v == &comp; };
+		const auto found = std::find_if(collisions_.crbegin(), collisions_.crend(), pr);
+		if (found != collisions_.crend()) collisions_.erase(found.base() - 1);
+	}
+
+	void CWorld::UpdateGame()
+	{
+		const auto deltaSeconds = UpdateTime();
+		
+		for (const auto& actor : actors_)
+			actor->Update(deltaSeconds);
+		
+		for (size_t i = 0; i < collisions_.size(); ++i)
+			for (auto j = i+1; j < collisions_.size(); ++j)
+				collisions_[i].get().TestOverlap(collisions_[j]);
+		
+		for (auto&& pending : pendingActors_)
+		{
+			auto& actor = *pending;
+			actors_.push_back(std::move(pending));
+			actor.BeginPlay();
+		}
+		pendingActors_.clear();
+
+		for (auto it = actors_.rbegin(); it != actors_.rend();)
+		{
+			const auto& actor = **it;
+			if (actor.GetState() == AActor::EState::dead)
+			{
+				const auto next = actors_.erase(it.base() - 1);
+				it = std::make_reverse_iterator(next);
+			}
+			else
+			{
+				++it;
+			}
+		}
+	}
+
+	float CWorld::UpdateTime()
+{
+		using namespace std::chrono;
+		const auto now = steady_clock::now();
+		const auto deltaSeconds = duration<float>{now - time_}.count();
+		time_ = now;
+		return deltaSeconds;
+	}
+
+	void CWorld::RegisterActor(std::unique_ptr<AActor>&& actor)
+	{
+		pendingActors_.push_back(std::move(actor));
+	}
+}
