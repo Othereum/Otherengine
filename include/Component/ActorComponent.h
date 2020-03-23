2fe9e83e5fe25b360a1c18cc5424eb3841d9593e
Othereum <seokjin.dev@gmail.com>
2020-03-23 16:33:46 +0900

    feat: Add is_enabled property to actor component

--- include/Component/ActorComponent.h
+++ include/Component/ActorComponent.h
@@ -14,8 +14,11 @@ namespace Game
 
 		virtual void BeginPlay() {}
 		virtual void Update(float deltaSeconds) {}
-		[[nodiscard]] int GetUpdateOrder() const noexcept { return updateOrder_; }
 		
+		void SetEnabled(bool enable) noexcept { is_enabled_ = enable; }
+		[[nodiscard]] bool IsEnabled() const noexcept { return is_enabled_; }
+		
+		[[nodiscard]] int GetUpdateOrder() const noexcept { return updateOrder_; }
 		[[nodiscard]] AActor& GetOwner() const noexcept { return owner_; }
 		[[nodiscard]] CEngine& GetEngine() const noexcept;
 		[[nodiscard]] CWorld& GetWorld() const noexcept;
@@ -23,5 +26,6 @@ namespace Game
 	private:
 		AActor& owner_;
 		int updateOrder_;
+		bool is_enabled_ = true;
 	};
 }
