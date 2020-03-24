e2614a120e892f8d33aeb7c6a25d0cad8f3fdce2
Othereum <seokjin.dev@gmail.com>
2020-03-24 17:15:57 +0900

    feat: Add newton physics

--- include/Component/MoveComponent.h
+++ include/Component/MoveComponent.h
@@ -8,17 +8,23 @@ namespace game
 	class CMoveComponent : public CActorComponent
 	{
 	public:
-		explicit CMoveComponent(AActor& owner, int updateOrder = 10);
+		explicit CMoveComponent(AActor& owner, int update_order = 10);
 
-		void Update(float deltaSeconds) override;
-		
+		void Update(float delta_seconds) override;
+
+		void SetMass(float mass) noexcept { mass_ = mass; }
+		void AddForce(FVector2 force) noexcept { force_ += force; }
 		void SetVelocity(FVector2 newvel) noexcept { vel_ = newvel; }
 		void SetAngularVelocity(Degrees newvel) noexcept { rvel_ = newvel; }
 
+		[[nodiscard]] float GetMass() const noexcept { return mass_; }
+		[[nodiscard]] FVector2 GetForce() const noexcept { return force_; }
 		[[nodiscard]] FVector2 GetVelocity() const noexcept { return vel_; }
 		[[nodiscard]] Degrees GetAngularVelocity() const noexcept { return rvel_; }
 
 	private:
+		float mass_ = 1;
+		FVector2 force_;
 		FVector2 vel_;
 		Degrees rvel_;
 	};
