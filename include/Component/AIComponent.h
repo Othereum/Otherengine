2c46b8fe2be342accf86f5cfd932a12523539556
Othereum <seokjin.dev@gmail.com>
2020-03-25 14:58:49 +0900

    feat: Add owner component reference

--- include/Component/AIComponent.h
+++ include/Component/AIComponent.h
@@ -11,11 +11,16 @@ namespace game
 		class Base
 		{
 		public:
+			explicit Base(class AIComponent& owner) noexcept: owner_{owner} {}
 			virtual ~Base() = default;
 			virtual void OnEnter(Base& prev) {}
 			virtual void Update(float delta_seconds) {}
 			virtual void OnExit(Base& next) {}
 			[[nodiscard]] virtual FName GetName() const = 0;
+			[[nodiscard]] AIComponent& GetOwner() const noexcept { return owner_; }
+
+		private:
+			AIComponent& owner_;
 		};
 	}
 	
