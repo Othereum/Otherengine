0e57eeb3ca5d3dacf811eb0a6b66bef7fd9073ec
Othereum <seokjin.dev@gmail.com>
2020-03-26 15:14:15 +0900

    docs: Add docs

--- include/Component/AIComponent.h
+++ include/Component/AIComponent.h
@@ -30,7 +30,13 @@ namespace game
 	public:
 		AIComponent(AActor& owner, int update_order = 100);
 		void Update(float delta_seconds) override;
+		
+		/**
+		 * \brief Change state. You must add state before change, or std::out_of_bound exception will be thrown
+		 * \param name New state name
+		 */
 		void ChangeState(FName name);
+		
 		void AddState(std::unique_ptr<ai_state::Base>&& state);
 
 		template <class T, class... Args>
