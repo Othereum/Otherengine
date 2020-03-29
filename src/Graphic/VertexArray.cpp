e440566c1733c2ece8f9b5c444134e67fdba37b1
Othereum <seokjin.dev@gmail.com>
2020-03-29 13:37:29 +0900

    feat: Index buffer

--- src/Graphic/VertexArray.cpp
+++ src/Graphic/VertexArray.cpp
@@ -7,9 +7,14 @@ namespace game
 	{
 		glGenVertexArrays(1, &vertex_array_);
 		glBindVertexArray(vertex_array_);
+		
 		glGenBuffers(1, &vertex_buffer_);
 		glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_);
 		glBufferData(GL_ARRAY_BUFFER, verts.size_bytes(), verts.data(), GL_STATIC_DRAW);
+
+		glGenBuffers(1, &index_buffer_);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size_bytes(), indices.data(), GL_STATIC_DRAW);
 	}
 
 	VertexArray::~VertexArray()
