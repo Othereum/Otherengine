64bbc59b9f4b79971d05a12dac6aceb4261dcc96
Othereum <seokjin.dev@gmail.com>
2020-03-30 12:44:33 +0900

    feat:: Add Shader

--- include/Graphics/Shader.h
+++ include/Graphics/Shader.h
@@ -1,6 +1,17 @@
 #pragma once
+#include <string_view>
 
 namespace game::graphics
 {
-	
+	class Shader
+	{
+	public:
+		Shader(std::string_view vert_name, std::string_view frag_name);
+		~Shader();
+
+	private:
+		unsigned vert_shader_;
+		unsigned frag_shader_;
+		unsigned shader_program_;
+	};
 }
