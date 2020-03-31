d205867b7a25f40f8ab932ea5246f3e6951171dd
Othereum <seokjin.dev@gmail.com>
2020-03-31 12:08:21 +0900

    feat: Error message

--- src/Graphics/Shader.cpp
+++ src/Graphics/Shader.cpp
@@ -8,7 +8,7 @@ namespace game::graphics
 	static std::string Open(std::string_view filename)
 	{
 		std::ifstream file{filename.data(), std::ios_base::in | std::ios_base::ate};
-		if (!file.is_open()) throw std::ios_base::failure{fmt::format("Shader file not found: {}", filename)};
+		if (!file.is_open()) throw std::ios_base::failure{fmt::format("Shader file not found: '{}'", filename)};
 
 		std::string code(file.tellg(), 0);
 		file.seekg(0);
