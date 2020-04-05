6dda13a9abf2f1bc002a97f88f5adcfecd641406
Othereum <seokjin.dev@gmail.com>
2020-04-05 14:12:39 +0900

    style: Add glm::

--- include/MathUtil.h
+++ include/MathUtil.h
@@ -10,12 +10,12 @@ namespace game::math
 	inline Radians Asin(float y) noexcept { return Radians{asinf(y)}; }
 	inline Radians Atan2(float y, float x) noexcept { return Radians{atan2f(y, x)}; }
 
-	inline Radians V2R(const vec2& v) noexcept
+	inline Radians V2R(const glm::vec2& v) noexcept
 	{
 		return Atan2(v.y, v.x);
 	}
 
-	inline vec2 R2V(Radians r) noexcept
+	inline glm::vec2 R2V(Radians r) noexcept
 	{
 		return {Cos(r), Sin(r)};
 	}
@@ -25,33 +25,33 @@ namespace game::math
 		return Radians{Rand(-kPi, kPi)};
 	}
 
-	inline vec2 RandUnitVec() noexcept
+	inline glm::vec2 RandUnitVec() noexcept
 	{
 		return R2V(RandAng());
 	}
 
-	inline vec2 RandVec(const vec2& min, const vec2& max) noexcept
+	inline glm::vec2 RandVec(const glm::vec2& min, const glm::vec2& max) noexcept
 	{
 		return {Rand(min.x, max.x), Rand(min.y, max.y)};
 	}
 
-	inline float GetRangePct(const vec2& range, float val) noexcept
+	inline float GetRangePct(const glm::vec2& range, float val) noexcept
 	{
 		return GetRangePct(range.x, range.y, val);
 	}
 
-	inline float GetRangeValue(const vec2& range, float pct) noexcept
+	inline float GetRangeValue(const glm::vec2& range, float pct) noexcept
 	{
 		return Lerp(range.x, range.y, pct);
 	}
 
-	inline float MapRngClamp(const vec2& in_rng, const vec2& out_rng, float val) noexcept
+	inline float MapRngClamp(const glm::vec2& in_rng, const glm::vec2& out_rng, float val) noexcept
 	{
 		const auto pct = Clamp(GetRangePct(in_rng, val), 0, 1);
 		return GetRangeValue(out_rng, pct);
 	}
 
-	inline float MapRng(const vec2& in_rng, const vec2& out_rng, float val) noexcept
+	inline float MapRng(const glm::vec2& in_rng, const glm::vec2& out_rng, float val) noexcept
 	{
 		return GetRangeValue(out_rng, GetRangePct(in_rng, val));
 	}
