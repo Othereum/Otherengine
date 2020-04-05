75fba990f94cffb187aba585be04b82c936eb697
Othereum <seokjin.dev@gmail.com>
2020-04-05 13:37:49 +0900

    refactor: Change type

--- include/MathUtil.h
+++ include/MathUtil.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Rotation.h"
-#include "Algebra.h"
 
 namespace game::math
 {
@@ -11,12 +10,12 @@ namespace game::math
 	inline Radians Asin(float y) noexcept { return Radians{asinf(y)}; }
 	inline Radians Atan2(float y, float x) noexcept { return Radians{atan2f(y, x)}; }
 
-	inline Radians V2R(Vector2f v) noexcept
+	inline Radians V2R(const vec2& v) noexcept
 	{
-		return Atan2(v.y(), v.x());
+		return Atan2(v.y, v.x);
 	}
 
-	inline Vector2f R2V(Radians r) noexcept
+	inline vec2 R2V(Radians r) noexcept
 	{
 		return {Cos(r), Sin(r)};
 	}
@@ -26,39 +25,34 @@ namespace game::math
 		return Radians{Rand(-kPi, kPi)};
 	}
 
-	inline Vector2f RandUnitVec() noexcept
+	inline vec2 RandUnitVec() noexcept
 	{
 		return R2V(RandAng());
 	}
 
-	inline Vector2f RandVec(const Vector2f& min, const Vector2f& max) noexcept
+	inline vec2 RandVec(const vec2& min, const vec2& max) noexcept
 	{
-		return {Rand(min.x(), max.x()), Rand(min.y(), max.y())};
+		return {Rand(min.x, max.x), Rand(min.y, max.y)};
 	}
 
-	inline float GetRangePct(const Vector2f& range, float val) noexcept
+	inline float GetRangePct(const vec2& range, float val) noexcept
 	{
-		return GetRangePct(range.x(), range.y(), val);
+		return GetRangePct(range.x, range.y, val);
 	}
 
-	inline float GetRangeValue(const Vector2f& range, float pct) noexcept
+	inline float GetRangeValue(const vec2& range, float pct) noexcept
 	{
-		return Lerp(range.x(), range.y(), pct);
+		return Lerp(range.x, range.y, pct);
 	}
 
-	inline float MapRngClamp(const Vector2f& in_rng, const Vector2f& out_rng, float val) noexcept
+	inline float MapRngClamp(const vec2& in_rng, const vec2& out_rng, float val) noexcept
 	{
 		const auto pct = Clamp(GetRangePct(in_rng, val), 0, 1);
 		return GetRangeValue(out_rng, pct);
 	}
 
-	inline float MapRng(const Vector2f& in_rng, const Vector2f& out_rng, float val) noexcept
+	inline float MapRng(const vec2& in_rng, const vec2& out_rng, float val) noexcept
 	{
 		return GetRangeValue(out_rng, GetRangePct(in_rng, val));
-	}
-
-	inline Matrix4f CreateScale(const Vector3f& scale) noexcept
-	{
-		
 	}
 }
