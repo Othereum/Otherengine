c510d601a332540257f93173e8d4110fcaef8261
Othereum <seokjin.dev@gmail.com>
2020-04-12 14:41:35 +0900

    refactor: Improve constructor

--- include/Matrix.h
+++ include/Matrix.h
@@ -5,17 +5,15 @@ namespace game
 	struct Mat4
 	{
 		template <class... Args>
-		explicit constexpr Mat4(Args&&... args) noexcept
-			:m{args...}
+		constexpr Mat4(Args... args) noexcept
+			:m{float(args)...}
 		{
 		}
 		
 		auto& operator[](size_t i) noexcept { return m[i]; }
 		auto& operator[](size_t i) const noexcept { return m[i]; }
 
-		Mat4 operator*(const Mat4& o) const noexcept
-		{
-		}
+		Mat4 operator*(const Mat4& o) const noexcept;
 		
 	private:
 		float m[4][4];
