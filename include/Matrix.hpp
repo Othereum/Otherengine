8dc6e9448c974a1467e2785d94da9e6868505268
Othereum <seokjin.dev@gmail.com>
2020-04-17 21:48:11 +0900

    feat: Generalization

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -1,53 +1,65 @@
 #pragma once
+#include <algorithm>
 
 namespace game
 {
-	struct Mat4
+	template <class T, size_t R, size_t C = R>
+	struct Matrix
 	{
-		static constexpr Mat4 identity()
+		static constexpr const Matrix& Identity()
 		{
-			return {
-				1, 0, 0, 0,
-				0, 1, 0, 0,
-				0, 0, 1, 0,
-				0, 0, 0, 1
-			};
+			static constexpr auto identity = []
+			{
+				Matrix matrix;
+				for (size_t i = 0; i < std::min(R, C); ++i)
+					matrix[i][i] = 1;
+				return matrix;
+			}();
+			
+			return identity;
+		}
+		
+		static constexpr const Matrix& Zero()
+		{
+			static constexpr Matrix zero;
+			return zero;
 		}
 		
 		template <class... Args>
-		constexpr Mat4(Args... args) noexcept
-			:m{float(args)...}
+		constexpr Matrix(Args... args) noexcept
+			:m{static_cast<float>(args)...}
 		{
 		}
 		
-		constexpr bool operator==(const Mat4&) const noexcept = default;
+		constexpr bool operator==(const Matrix&) const noexcept = default;
 		
 		constexpr auto& operator[](size_t i) noexcept { return m[i]; }
 		constexpr auto& operator[](size_t i) const noexcept { return m[i]; }
 
-		constexpr Mat4 operator+(const Mat4& b) const noexcept { auto c = *this; return c += b; }
-		constexpr Mat4& operator+=(const Mat4& b) & noexcept
+		constexpr Matrix operator+(const Matrix& b) const noexcept { auto c = *this; return c += b; }
+		constexpr Matrix& operator+=(const Matrix& b) & noexcept
 		{
-			for (auto i=0; i<16; ++i) m[0][i] += b.m[0][i];
+			for (auto i=0; i<R*C; ++i) m[0][i] += b.m[0][i];
 			return *this;
 		}
 
-		constexpr Mat4 operator-(const Mat4& b) const noexcept { auto c = *this; return c -= b; }
-		constexpr Mat4& operator-=(const Mat4& b) & noexcept
+		constexpr Matrix operator-(const Matrix& b) const noexcept { auto c = *this; return c -= b; }
+		constexpr Matrix& operator-=(const Matrix& b) & noexcept
 		{
-			for (auto i=0; i<16; ++i) m[0][i] -= b.m[0][i];
+			for (auto i=0; i<R*C; ++i) m[0][i] -= b.m[0][i];
 			return *this;
 		}
 
-		constexpr Mat4 operator*(float f) const noexcept { auto c = *this; return c *= f; }
-		constexpr Mat4& operator*=(float f) & noexcept
+		constexpr Matrix operator*(float f) const noexcept { auto c = *this; return c *= f; }
+		constexpr Matrix& operator*=(float f) & noexcept
 		{
-			for (auto i=0; i<16; ++i) m[0][i] *= f;
+			for (auto i=0; i<R*C; ++i) m[0][i] *= f;
 			return *this;
 		}
 
-		constexpr Mat4 operator*(const Mat4& b) const noexcept
+		constexpr Matrix operator*(const Matrix& b) const noexcept
 		{
+			/*
 			return {
 				m[0][0]*b[0][0]+m[0][1]*b[1][0]+m[0][2]*b[2][0]+m[0][3]*b[3][0], m[0][0]*b[0][1]+m[0][1]*b[1][1]+m[0][2]*b[2][1]+m[0][3]*b[3][1],
 				m[0][0]*b[0][2]+m[0][1]*b[1][2]+m[0][2]*b[2][2]+m[0][3]*b[3][2], m[0][0]*b[0][3]+m[0][1]*b[1][3]+m[0][2]*b[2][3]+m[0][3]*b[3][3],
@@ -61,18 +73,19 @@ namespace game
 				m[3][0]*b[0][0]+m[3][1]*b[1][0]+m[3][2]*b[2][0]+m[3][3]*b[3][0], m[3][0]*b[0][1]+m[3][1]*b[1][1]+m[3][2]*b[2][1]+m[3][3]*b[3][1],
 				m[3][0]*b[0][2]+m[3][1]*b[1][2]+m[3][2]*b[2][2]+m[3][3]*b[3][2], m[3][0]*b[0][3]+m[3][1]*b[1][3]+m[3][2]*b[2][3]+m[3][3]*b[3][3]
 			};
+			*/
 		}
-		constexpr Mat4& operator*=(const Mat4& b) & noexcept { return *this = *this * b; }
+		constexpr Matrix& operator*=(const Matrix& b) & noexcept { return *this = *this * b; }
 
-		constexpr void transpose() noexcept { *this = transposed(); }
-		[[nodiscard]] constexpr Mat4 transposed() const noexcept
+		constexpr std::enable_if_t<R==C> transpose() noexcept { *this = transposed(); }
+		[[nodiscard]] constexpr Matrix<T, C, R> transposed() const noexcept
 		{
-			Mat4 t;
-			for (auto i=0; i<4; ++i) for (auto j=0; j<4; ++j) t[i][j] = m[j][i];
+			Matrix<T, C, R> t;
+			for (auto i=0; i<R; ++i) for (auto j=0; j<C; ++j) t[j][i] = m[i][j];
 			return t;
 		}
 
 	private:
-		float m[4][4];
+		T m[R][C];
 	};
 }
