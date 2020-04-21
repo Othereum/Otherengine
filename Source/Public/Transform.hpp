41cfa16aece620767c5629c4aed86e1d13e39669
Othereum <seokjin.dev@gmail.com>
2020-04-21 17:13:10 +0900

    feat: Add constructor

--- Source/Public/Transform.hpp
+++ Source/Public/Transform.hpp
@@ -3,13 +3,18 @@
 #include "Rotation.h"
 #include "MathUtil.h"
 
-namespace game
+namespace oeng
 {
 	struct Transform
 	{
 		Vec2 pos;
 		Degrees rot;
 		Vec2 scale;
+
+		explicit constexpr Transform(const Vec2& p = Vec2::Zero(), Degrees r = 0_deg, const Vec2& s = Vec2::One()) noexcept:
+			pos{p}, rot{r}, scale{s}
+		{
+		}
 
 		[[nodiscard]] constexpr Mat4 ToMatrix() const noexcept
 		{
