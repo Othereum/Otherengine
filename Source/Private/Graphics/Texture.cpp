8d673b975fe01b6f2712ee86435524a6f0c7bc60
Othereum <seokjin.dev@gmail.com>
2020-04-23 15:24:41 +0900

    feat: Create Texture class

---
+++ Source/Private/Graphics/Texture.cpp
@@ -0,0 +1,34 @@
+#include "Texture.hpp"
+
+#include <GL/glew.h>
+#include <SOIL2/SOIL2.h>
+
+#include "StringUtil.hpp"
+
+namespace oeng::graphics
+{
+	Texture::Texture(const char* name)
+	{
+		Vector<int, 2> size;
+		auto num_channels = 0;
+
+		const std::unique_ptr<unsigned char[]> image{SOIL_load_image(name, &size.x, &size.y, &num_channels, SOIL_LOAD_AUTO)};
+		if (!image) throw std::runtime_error{str::Concat("Failed to load image ", name, ": ", SOIL_last_result())};
+
+		const auto format = num_channels == 4 ? GL_RGBA : GL_RGB;
+		size_ = size;
+
+		glGenTextures(1, &id_);
+		glBindTexture(GL_TEXTURE_2D, id_);
+		
+		glTexImage2D(GL_TEXTURE_2D, 0, format, size.x, size.y, 0, format, GL_UNSIGNED_BYTE, image.get());
+		
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	}
+
+	Texture::~Texture()
+	{
+		glDeleteTextures(1, &id_);
+	}
+}
---
+++ Source/Private/Graphics/Texture.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "Vector.hpp"
+
+namespace oeng::graphics
+{
+	class Texture
+	{
+	public:
+		explicit Texture(const char* name);
+		~Texture();
+
+		[[nodiscard]] auto& Size() const noexcept { return size_; }
+
+		Texture(const Texture&) = delete;
+		Texture(Texture&&) = delete;
+		Texture& operator=(const Texture&) = delete;
+		Texture& operator=(Texture&&) = delete;
+
+	private:
+		unsigned id_ = 0;
+		Vector<uint16_t, 2> size_;
+	};
+}
