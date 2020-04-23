a0178c60cabb571645e8fe2160646694f2137fae
Othereum <seokjin.dev@gmail.com>
2020-04-23 17:40:10 +0900

    feat: Add Vertex structure and uv data

--- Source/Private/Graphics/VertexArray.cpp
+++ Source/Private/Graphics/VertexArray.cpp
@@ -3,21 +3,25 @@
 
 namespace oeng::graphics
 {
-	VertexArray::VertexArray(gsl::span<const Vec3> verts, gsl::span<const Vector<uint16_t, 3>> indices)
+	VertexArray::VertexArray(const Vertex* verts, size_t num_verts, const Vector<uint16_t, 3>* indices, size_t num_indices)
 	{
 		glGenVertexArrays(1, &vertex_array_);
 		glBindVertexArray(vertex_array_);
 		
 		glGenBuffers(1, &vertex_buffer_);
 		glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_);
-		glBufferData(GL_ARRAY_BUFFER, verts.size_bytes(), verts.data(), GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof Vertex * num_verts, verts, GL_STATIC_DRAW);
 
 		glGenBuffers(1, &index_buffer_);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size_bytes(), indices.data(), GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof Vector<uint16_t, 3> * num_indices, indices, GL_STATIC_DRAW);
 
 		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof Vec3, nullptr);
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof Vertex, nullptr);
+
+		constexpr auto offset = reinterpret_cast<const void*>(offsetof(Vertex, uv));
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, false, sizeof Vertex, offset);
 	}
 
 	VertexArray::~VertexArray()
--- Source/Private/Graphics/VertexArray.h
+++ Source/Private/Graphics/VertexArray.h
@@ -1,18 +1,37 @@
 #pragma once
-#include <gsl/span>
 #include "Vector.hpp"
 
 namespace oeng::graphics
 {
+	struct Vertex
+	{
+		Vec3 pos;
+		Vec2 uv;
+
+		constexpr Vertex() noexcept = default;
+		constexpr Vertex(Vec3 pos, Vec2 uv) noexcept: pos{pos}, uv{uv} {}
+	};
+	
 	class VertexArray
 	{
 	public:
-		VertexArray(gsl::span<const Vec3> verts, gsl::span<const Vector<uint16_t, 3>> indices);
+		template <size_t NumVerts, size_t NumIndices>
+		VertexArray(const Vertex (&verts)[NumVerts], const Vector<uint16_t, 3> (&indices)[NumIndices])
+			:VertexArray{verts, NumVerts, indices, NumIndices}
+		{
+		}
+		
+		VertexArray(const Vertex* verts, size_t num_verts, const Vector<uint16_t, 3>* indices, size_t num_indices);
 		~VertexArray();
 
+		VertexArray(const VertexArray&) = delete;
+		VertexArray(VertexArray&&) = delete;
+		VertexArray& operator=(const VertexArray&) = delete;
+		VertexArray& operator=(VertexArray&&) = delete;
+
 	private:
-		unsigned vertex_buffer_;
-		unsigned index_buffer_;
-		unsigned vertex_array_;
+		unsigned vertex_buffer_ = 0;
+		unsigned index_buffer_ = 0;
+		unsigned vertex_array_ = 0;
 	};
 }
