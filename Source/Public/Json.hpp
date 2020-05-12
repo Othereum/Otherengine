61c42b665e0017ca77ee1d76d03cd40bf017fe25
Othereum <seokjin.dev@gmail.com>
2020-05-12 16:22:30 +0900

    feat: Vector json

--- Source/Public/Json.hpp
+++ Source/Public/Json.hpp
@@ -1,3 +1,19 @@
 #pragma once
 #include "JsonFwd.hpp"
 #include <nlohmann/json.hpp>
+#include <otm/Vector.hpp>
+
+namespace otm
+{
+	template <class T, size_t L>
+	void to_json(oeng::Json& json, const Vector<T, L>& v)
+	{
+		json = v.data;
+	}
+
+	template <class T, size_t L>
+	void from_json(const oeng::Json& json, Vector<T, L>& v)
+	{
+		json.get_to(v.data);
+	}
+}
