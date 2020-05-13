abc2e74922b2a77e4bb6ecc7f259ca85a0a80c8c
Othereum <seokjin.dev@gmail.com>
2020-05-13 14:56:13 +0900

    feat: Explicitly delete copy/move functions

--- Source/Public/Engine.hpp
+++ Source/Public/Engine.hpp
@@ -38,6 +38,11 @@ namespace oeng
 		[[nodiscard]] CInputSystem& GetInputSystem() const noexcept { return *input_system_; }
 		[[nodiscard]] Renderer& GetRenderer() const noexcept { return *renderer_; }
 		[[nodiscard]] Vec2u16 GetScreenSize() const noexcept;
+
+		Engine(const Engine&) = delete;
+		Engine(Engine&&) = delete;
+		Engine& operator=(const Engine&) = delete;
+		Engine& operator=(Engine&&) = delete;
 		
 	private:
 		void Tick();
