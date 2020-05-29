39c832be42be1df651f42f2aaa13777da8f60d4c
Othereum <seokjin.dev@gmail.com>
2020-05-29 16:32:51 +0900

    feat: Implement Allocate()

--- Source/Public/Memory.hpp
+++ Source/Public/Memory.hpp
@@ -14,27 +14,31 @@ namespace oeng
 			return pool;
 		}
 
-		template <class T = void>
-		T* Allocate(size_t count)
+		template <class T>
+		T* Allocate(const size_t count)
 		{
+			const auto need_bytes = count * sizeof T;
+			const auto need_blocks = need_bytes/BlockSize + otm::Min(1, need_bytes%BlockSize);
+			
 			for (auto& c : containers_)
 			{
-				if (c.remaining < count) continue;
+				if (c.remaining * BlockSize < need_bytes) continue;
 				
-				size_t bytes = 0;
-				for (size_t i = 0; i < c.blocks.size(); ++i)
+				size_t continuous = 0;
+				for (size_t cur_block = c.first_available; cur_block < c.blocks.size(); ++cur_block)
 				{
-					if (c.occupied[i]) bytes = 0;
-					else if ((bytes += BlockSize) >= count * sizeof T)
+					if (c.occupied[cur_block]) continuous = 0;
+					else if (++continuous * BlockSize >= need_bytes)
 					{
-						const auto cnt = bytes/BlockSize;
-						const auto idx = i - cnt + 1;
-						c.remaining -= cnt;
-						for (auto j = idx; j <= i; ++j) c.occupied[j] = true;
-						return reinterpret_cast<T*>(c.blocks.data() + idx);
+						const auto begin = cur_block - need_blocks + 1;
+						return Allocate<T>(c, begin, need_blocks);
 					}
 				}
 			}
+
+			const auto new_blocks = otm::Max(total_blocks_ / 2, need_blocks);
+			total_blocks_ += new_blocks;
+			return Allocate<T>(containers_.emplace_back(new_blocks), 0, need_blocks);
 		}
 		
 		void Deallocate(void* ptr, size_t count)
@@ -68,14 +72,22 @@ namespace oeng
 			size_t remaining;
 		};
 
-		explicit MemoryPool(size_t initial_size)
-			:total_cnt_{initial_size}
+		template <class T>
+		static T* Allocate(BlockContainer& c, size_t begin, size_t blocks)
 		{
-			containers_.emplace_back(initial_size);
+			const auto end = begin + blocks;
+			auto i = begin;
+			for (; i < end; ++i) c.occupied[i] = true;
+			for (; i < c.blocks.size() && c.occupied[i]; ++i) {}
+			c.first_available = i;
+			c.remaining -= blocks;
+			return reinterpret_cast<T*>(c.blocks.data() + begin);
 		}
 
+		MemoryPool() = default;
+		
 		std::vector<BlockContainer> containers_;
-		size_t total_cnt_;
+		size_t total_blocks_ = 0;
 	};
 
 	template <class T>
