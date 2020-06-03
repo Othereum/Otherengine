0f7592d59282afadf39b0f1139bb9b546ec46f7f
Othereum <seokjin.dev@gmail.com>
2020-06-03 10:36:02 +0900

    feat: OE_BUILD_STATIC

--- Source/Public/API.hpp
+++ Source/Public/API.hpp
@@ -1,17 +1,21 @@
 #pragma once
 
-#ifdef _WIN32
-# if OE_BUILD
-#  define OEAPI __declspec(dllexport)
-# else
-#  define OEAPI __declspec(dllimport)
-# endif
+#if OE_BUILD_STATIC
+	#define OEAPI
 #else
-# if defined(__GNUC__) && __GNUC__>=4
-#  define OEAPI __attribute__ ((visibility("default")))
-# elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
-#  define OEAPI __global
-# else
-#  define OEAPI
-# endif
+	#ifdef _WIN32
+		#if OE_BUILD
+			#define OEAPI __declspec(dllexport)
+		#else
+			#define OEAPI __declspec(dllimport)
+		#endif
+	#else
+		#if defined(__GNUC__) && __GNUC__>=4
+			#define OEAPI __attribute__ ((visibility("default")))
+		#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+			#define OEAPI __global
+		#else
+			#define OEAPI
+		#endif
+	#endif
 #endif
