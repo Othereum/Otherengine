8ede4c70c16a9d73e930931ef5ac89076eeeb8d9
Othereum <seokjin.dev@gmail.com>
2020-06-04 23:22:19 +0900

    feat: Add TEXT() macro

--- Source/Public/Templates/String.hpp
+++ Source/Public/Templates/String.hpp
@@ -2,6 +2,8 @@
 #include <string>
 #include "Memory.hpp"
 
+#define TEXT(x) L ## x
+
 namespace oeng
 {
 	template <class CharT, class Traits = std::char_traits<CharT>, class Alloc = RawAllocator<CharT>>
