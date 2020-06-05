e4d3ddfccd90bcfc8f2bf1a6bff182d8b06a214c
Othereum <seokjin.dev@gmail.com>
2020-06-05 09:12:29 +0900

    refactor: Remove TEXT macro and Char type alias
    generic character type support is too complicated. Just use char and UTF-8

--- Source/Public/Templates/String.hpp
+++ Source/Public/Templates/String.hpp
@@ -2,13 +2,10 @@
 #include <string>
 #include "Memory.hpp"
 
-#define TEXT(x) L ## x
-
 namespace oeng
 {
 	template <class CharT, class Traits = std::char_traits<CharT>, class Alloc = RawAllocator<CharT>>
 	using BasicString = std::basic_string<CharT, Traits, Alloc>;
 
-	using Char = char;
-	using String = BasicString<Char>;
+	using String = BasicString<char>;
 }
