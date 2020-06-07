8399bcb9885db3f7466d28133f104e1545203c60
Othereum <seokjin.dev@gmail.com>
2020-06-08 01:02:33 +0900

    feat: Better error message

--- Source/Private/Json.cpp
+++ Source/Private/Json.cpp
@@ -1,12 +1,19 @@
 #include "Json.hpp"
 #include <fstream>
+#include "Format.hpp"
 
 namespace oeng
 {
 	OEAPI Json ReadFileAsJson(Path file)
 	{
 		Json json;
-		std::ifstream{file} >> json;
+		std::ifstream is{file};
+		if (!is.is_open()) throw std::runtime_error{format("Can't open file '{}'", file->string())};
+		try { is >> json; }
+		catch (const std::exception& e)
+		{
+			throw std::runtime_error{format("Exception thrown while parsing '{}': {}", file->string(), e.what())};
+		}
 		return json;
 	}
 }
