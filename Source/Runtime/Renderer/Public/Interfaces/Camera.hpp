9f8d22d5158db787c452df881d18d20537c0f36b
Othereum <seokjin.dev@gmail.com>
2020-06-09 16:06:10 +0900

    feat: Better interface for cache

--- Source/Runtime/Renderer/Public/Interfaces/Camera.hpp
+++ Source/Runtime/Renderer/Public/Interfaces/Camera.hpp
@@ -6,7 +6,8 @@ namespace oeng
 	class ICamera
 	{
 	public:
-		[[nodiscard]] virtual Mat4 GetViewProj(Vec2u16 scr) const noexcept = 0;
+		[[nodiscard]] virtual Mat4 GetViewProj() noexcept = 0;
+		virtual void OnScreenSizeChanged(Vec2u16 scr) noexcept = 0;
 		
 		constexpr ICamera() noexcept = default;
 		virtual ~ICamera() = default;
