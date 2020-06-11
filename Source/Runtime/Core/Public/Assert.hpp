48e2ce3b70e805d4ddb4a755a72993ff9c62e46c
Othereum <seokjin.dev@gmail.com>
2020-06-11 11:08:13 +0900

    fix: Name collision

--- Source/Runtime/Core/Public/Assert.hpp
+++ Source/Runtime/Core/Public/Assert.hpp
@@ -1,31 +1,30 @@
 #pragma once
+#include "API.hpp"
 
 #define STRINGIZE_IMPL(x) #x
 #define STRINGIZE(x) STRINGIZE_IMPL(x)
 #define LINE_STRING STRINGIZE(__LINE__)
 
-#if OE_BUILD_MODE == 0
+#if OE_BUILD_CONFIG == 0
 
 #define OE_DO_CHECK 1
 #define OE_DO_SLOW_CHECK 1
 
-#elif OE_BUILD_MODE == 1
+#elif OE_BUILD_CONFIG == 1
 
 #define OE_DO_CHECK 1
 #define OE_DO_SLOW_CHECK 0
 
-#elif OE_BUILD_MODE == 2
+#elif OE_BUILD_CONFIG == 2
 
 #define OE_DO_CHECK 0
 #define OE_DO_SLOW_CHECK 0
 
 #else
 
-#error OE_BUILD_MODE must be either 0, 1, or 2
+#error OE_BUILD_CONFIG must be either 0, 1, or 2
 
 #endif
-#include "API.hpp"
-
 
 #if OE_DO_CHECK
 
