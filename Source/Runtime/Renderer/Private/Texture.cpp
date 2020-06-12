7b7a4c9050d17a6ec03e4c1ccb1242306187b93e
Othereum <seokjin.dev@gmail.com>
2020-06-12 22:58:12 +0900

    feat: Add support of default texture

--- Source/Runtime/Renderer/Private/Texture.cpp
+++ Source/Runtime/Renderer/Private/Texture.cpp
@@ -4,36 +4,57 @@
 
 namespace oeng
 {
-	Texture::Texture(Path file)
+	Texture::Texture(Path path)
+	{
+		LoadInternal(path);
+	}
+
+	Texture::Texture(Texture&& r) noexcept
+		:id_{r.id_}, size_{r.size_}
+	{
+		r.id_ = 0;
+		r.size_ = {1, 1};
+	}
+
+	Texture::~Texture()
+	{
+		unsigned err;
+		// glDelete silently ignores 0
+		gl(err, glDeleteTextures, 1, &id_);
+	}
+
+	Texture& Texture::operator=(Texture&& r) noexcept
+	{
+		Texture{std::move(r)}.swap(*this);
+		return *this;
+	}
+
+	void Texture::LoadInternal(Path path)
 	{
 		Vector<int, 2> size;
 		auto num_channels = 0;
+		
+		const std::unique_ptr<const unsigned char[]> image{
+			SOIL_load_image(path->string().c_str(), &size[0], &size[1], &num_channels, SOIL_LOAD_AUTO)
+		};
+		if (!image) throw std::runtime_error{SOIL_last_result()};
 
-		auto str = file->string();
-		const std::unique_ptr<unsigned char[]> image{SOIL_load_image(str.c_str(), &size[0], &size[1], &num_channels, SOIL_LOAD_AUTO)};
-		if (!image) throw std::runtime_error{format("Failed to load image '{}': {}", str, SOIL_last_result())};
-
-		size_ = Vec2u16{size};
-
-		gl(glGenTextures, 1, &id_);
-		Activate();
+		constexpr auto max_size = std::numeric_limits<uint16_t>::max();
+		if (size[0] > max_size || size[1] > max_size)
+			throw std::runtime_error{format("Too big ({0}x{1}). Max size is {2}x{2}", size[0], size[1], max_size)};
 
 		int img_format;
 		int alignment;
-		
 		switch (num_channels)
 		{
-		case 3:
-			img_format = GL_RGB;
-			alignment = 1;
-			break;
-		case 4:
-			img_format = GL_RGBA;
-			alignment = 4;
-			break;
-		default:
-			throw std::runtime_error{format("Invalid image format for '{}'. num_channels was {}", str, num_channels)};
+		case 3: img_format = GL_RGB; alignment = 1; break;
+		case 4: img_format = GL_RGBA; alignment = 4; break;
+		default: throw std::runtime_error{format("Invalid format: num_channels was {}. Must be 3 or 4", num_channels)};
 		}
+
+		size_ = Vec2u16{size};
+		gl(glGenTextures, 1, &id_);
+		Activate();
 
 		gl(glPixelStorei, GL_UNPACK_ALIGNMENT, alignment);
 		gl(glTexImage2D, GL_TEXTURE_2D, 0, img_format, size[0], size[1], 0, img_format, GL_UNSIGNED_BYTE, image.get());
@@ -42,14 +63,20 @@ namespace oeng
 		gl(glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	}
 
-	Texture::~Texture()
+	void Texture::Load(Path path)
 	{
-		unsigned err;
-		gl(err, glDeleteTextures, 1, &id_);
+		Texture{path}.swap(*this);
 	}
 
 	void Texture::Activate() const
 	{
 		gl(glBindTexture, GL_TEXTURE_2D, id_);
+	}
+
+	void Texture::swap(Texture& r) noexcept
+	{
+		using std::swap;
+		swap(id_, r.id_);
+		swap(size_, r.size_);
 	}
 }
--- Source/Runtime/Renderer/Public/Texture.hpp
+++ Source/Runtime/Renderer/Public/Texture.hpp
@@ -7,19 +7,45 @@ namespace oeng
 	class OEAPI Texture
 	{
 	public:
-		explicit Texture(Path file);
+		/**
+		 * \brief Default black texture of 1x1 size. It can be used for rendering, just like normal loaded textures.
+		 */
+		constexpr Texture() noexcept = default;
+
+		/**
+		 * \brief Load texture from image file (png, jpg, ...)
+		 * \param path Image file path
+		 * \throw std::runtime_error If failed to load texture
+		 */
+		explicit Texture(Path path);
+		
+		Texture(Texture&& r) noexcept;
+		Texture(const Texture&) = delete;
 		~Texture();
 
-		void Activate() const;
-		[[nodiscard]] auto& Size() const noexcept { return size_; }
-
-		Texture(const Texture&) = delete;
-		Texture(Texture&&) = delete;
+		Texture& operator=(Texture&& r) noexcept;
 		Texture& operator=(const Texture&) = delete;
-		Texture& operator=(Texture&&) = delete;
+
+		/**
+		 * \brief Load texture from image file (png, jpg, ...)
+		 * \param path Image file path
+		 * \throw std::runtime_error If failed to load texture
+		 */
+		void Load(Path path);
+		
+		void Activate() const;
+		
+		[[nodiscard]] bool Loaded() const noexcept { return id_ != 0; }
+		[[nodiscard]] Vec2u16 Size() const noexcept { return size_; }
+
+		void swap(Texture& r) noexcept;
 
 	private:
+		void LoadInternal(Path path);
+
 		unsigned id_ = 0;
-		Vec2u16 size_;
+		Vec2u16 size_{1, 1};
 	};
+
+	inline void swap(Texture& a, Texture& b) noexcept { a.swap(b); }
 }
