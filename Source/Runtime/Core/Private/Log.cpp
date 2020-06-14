94352e05ff1e47edebb04780f48669e699a3fde5
Othereum <seokjin.dev@gmail.com>
2020-06-14 19:49:25 +0900

    feat: Conditional thread-safe

--- Source/Runtime/Core/Private/Log.cpp
+++ Source/Runtime/Core/Private/Log.cpp
@@ -3,15 +3,26 @@
 #include <spdlog/async.h>
 #include <spdlog/sinks/daily_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include "Templates/Sync.hpp"
 
 namespace oeng::log
 {
+	static constexpr bool kThreadSafe = OE_LOG_THREADSAFE;
+	
 	static std::shared_ptr<spdlog::logger> InitializeLogger()
 	{
 		std::filesystem::create_directory("../Logs");
+
+		using Daily = std::conditional_t<kThreadSafe,
+			spdlog::sinks::daily_file_sink_mt,
+			spdlog::sinks::daily_file_sink_st>;
 		
-		auto daily_file = std::make_shared<spdlog::sinks::daily_file_sink_st>("../Logs/Log.log", 0, 0);
-		auto stdout_color = std::make_shared<spdlog::sinks::stdout_color_sink_st>();
+		using Stdout = std::conditional_t<kThreadSafe,
+			spdlog::sinks::stdout_color_sink_mt,
+			spdlog::sinks::stdout_color_sink_st>;
+
+		auto daily_file = std::make_shared<Daily>("../Logs/Log.log", 0, 0);
+		auto stdout_color = std::make_shared<Stdout>();
 		
 		spdlog::sinks_init_list list{std::move(daily_file), std::move(stdout_color)};
 		auto logger_ptr = std::make_shared<spdlog::logger>(std::string{}, list);
