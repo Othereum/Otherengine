b3f7663ceb1e014da5a346d5e488f17327a4bf3e
Othereum <seokjin.dev@gmail.com>
2020-06-15 21:56:51 +0900

    feat: Add Config interface

--- Source/Runtime/Core/Public/Interfaces/Engine.hpp
+++ Source/Runtime/Core/Public/Interfaces/Engine.hpp
@@ -17,9 +17,22 @@ namespace oeng
 		[[nodiscard]] virtual World& GetWorld() noexcept = 0;
 		[[nodiscard]] virtual InputSystem& GetInputSystem() noexcept = 0;
 		[[nodiscard]] virtual Renderer& GetRenderer() noexcept = 0;
-		[[nodiscard]] virtual Vec2u16 GetScreenSize() const noexcept = 0;
+		[[nodiscard]] virtual Vec2u16 GetWindowSize() const noexcept = 0;
 		[[nodiscard]] virtual std::string_view GetGameName() const noexcept = 0;
-		[[nodiscard]] virtual const Json& GetConfig(Name name) const noexcept = 0;
+
+		/**
+		 * \brief Find config or create if not found.
+		 * \param name Config name
+		 * \return Reference to non-const json config object
+		 */
+		[[nodiscard]] virtual Json& Config(Name name) = 0;
+		
+		/**
+		 * \brief Save config as file. Existing files will be overwritten.
+		 * \param name Config name
+		 * \return true if successful
+		 */
+		virtual bool SaveConfig(Name name) noexcept = 0;
 		
 		constexpr IEngine() noexcept = default;
 		virtual ~IEngine() = default;
