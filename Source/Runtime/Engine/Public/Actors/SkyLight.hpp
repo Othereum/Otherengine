cd766cf334d9081953cabe2680e80ae54272dcf6
Othereum <seokjin.dev@gmail.com>
2020-06-15 11:56:45 +0900

    feat: Add ASkyLight

---
+++ Source/Runtime/Engine/Public/Actors/SkyLight.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "Actors/Actor.hpp"
+
+namespace oeng
+{
+	class SkyLightComponent;
+	
+	class OEAPI ASkyLight : public AActor
+	{
+	public:
+		explicit ASkyLight(World& world);
+
+		void Activate() const noexcept;
+		
+		void SetColor(const Vec3& color) const noexcept;
+		[[nodiscard]] const Vec3& GetColor() const noexcept;
+
+	private:
+		SkyLightComponent& light_;
+	};
+}
