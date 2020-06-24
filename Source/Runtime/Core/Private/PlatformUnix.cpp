65a29b37f5057f8cb58d4f4aa996f65d3cde9038
Othereum <seokjin.dev@gmail.com>
2020-06-24 09:50:25 +0900

    feat: Impl dll unix

--- Source/Runtime/Core/Private/PlatformUnix.cpp
+++ Source/Runtime/Core/Private/PlatformUnix.cpp
@@ -2,9 +2,31 @@
 
 #include "Platform.hpp"
 #include <cpuid.h>
+#include <dlfcn.h>
 
 namespace oeng::plf
 {
+	Dll::Dll(const char* filepath) try
+		:dll_{dlopen(filepath, RTLD_LAZY), &dlclose}
+	{
+	}
+	catch (const std::invalid_argument&)
+	{
+		throw std::runtime_error{dlerror()};
+	}
+
+	void* Dll::GetSymbol(const char* name) const
+	{
+		auto* const sym = FindSymbol(name);
+		if (!sym) throw std::runtime_error{dlerror()};
+		return sym;
+	}
+
+	void* Dll::FindSymbol(const char* name) const noexcept
+	{
+		return dlsym(dll_.get(), name);
+	}
+	
 	void CpuId(int cpu_info[4], int func_id) noexcept
 	{
 		auto* const info = reinterpret_cast<unsigned*>(cpu_info);
