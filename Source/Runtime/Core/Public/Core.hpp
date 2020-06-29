278d6f43bbf45be7d33ec03e4bd33c3d2116287e
Othereum <seokjin.dev@gmail.com>
2020-06-29 16:08:13 +0900

    chore: No static library

--- Source/Runtime/Core/Public/Core.hpp
+++ Source/Runtime/Core/Public/Core.hpp
@@ -2,32 +2,26 @@
 // ReSharper disable CppClangTidyBugproneMacroParentheses
 #pragma once
 
-#if OE_BUILD_STATIC
-	#define OEAPI
-	#define OE_EXPORT
-	#define OE_IMPORT
-#else
-	#ifdef _WIN32
-		#define OE_EXPORT __declspec(dllexport)
-		#define OE_IMPORT __declspec(dllimport)
+#ifdef _WIN32
+	#define OE_EXPORT __declspec(dllexport)
+	#define OE_IMPORT __declspec(dllimport)
 
-		#ifdef OE_BUILD
-			#define OEAPI OE_EXPORT
-		#else
-			#define OEAPI OE_IMPORT
-		#endif
+	#ifdef OE_BUILD
+		#define OEAPI OE_EXPORT
 	#else
-		#if defined(__GNUC__) && __GNUC__>=4
-			#define OEAPI __attribute__ ((visibility("default")))
-		#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
-			#define OEAPI __global
-		#else
-			#define OEAPI
-		#endif
-
-		#define OE_IMPORT OEAPI
-		#define OE_EXPORT OEAPI
+		#define OEAPI OE_IMPORT
 	#endif
+#else
+	#if defined(__GNUC__) && __GNUC__>=4
+		#define OEAPI __attribute__ ((visibility("default")))
+	#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+		#define OEAPI __global
+	#else
+		#define OEAPI
+	#endif
+
+	#define OE_IMPORT OEAPI
+	#define OE_EXPORT OEAPI
 #endif
 
 #define OE_EXPLICIT_CPMV(name, spec) \
