0cef04e103a4567985a96d94254e5e0ad1565775
Othereum <seokjin.dev@gmail.com>
2020-07-01 18:40:12 +0900

    feat: conversion to const char*

--- Source/Runtime/Core/Public/Path.hpp
+++ Source/Runtime/Core/Public/Path.hpp
@@ -30,9 +30,12 @@ namespace oeng
 		bool operator>=(const Path& r) const noexcept { return p >= r.p; }
 
 		[[nodiscard]] Name AsName() const noexcept { return p->first; }
+		[[nodiscard]] const char* CStr() const noexcept { return p->first->c_str(); }
 		[[nodiscard]] const Name::Str& Str() const noexcept { return p->first; }
-		operator Name() const noexcept { return p->first; }
-		operator const Name::Str&() const noexcept { return p->first; }
+
+		operator Name() const noexcept { return AsName(); }
+		operator const char*() const noexcept { return CStr(); }
+		operator const Name::Str&() const noexcept { return Str(); }
 		operator const std::filesystem::path&() const noexcept { return p->second; }
 		const std::filesystem::path& operator*() const noexcept { return p->second; }
 		const std::filesystem::path* operator->() const noexcept { return &p->second; }
