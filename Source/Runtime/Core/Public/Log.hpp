9da9224911d39880ae1e84039790d63e28c534ca
Othereum <seokjin.dev@gmail.com>
2020-07-08 09:08:41 +0900

    remove spdlog from public header

--- Source/Runtime/Core/Public/Log.hpp
+++ Source/Runtime/Core/Public/Log.hpp
@@ -1,66 +1,60 @@
 #pragma once
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable:4566 4819)
-#endif
-#include <spdlog/logger.h>
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
 #include "Core.hpp"
 #include "Format.hpp"
 #include "Templates/Time.hpp"
 
 namespace oeng::log
 {
-	namespace level = spdlog::level;
+	enum class Level
+	{
+	    kTrace, kDebug, kInfo, kWarn, kErr, kCritical, kOff,
+	};
 	
-	OEAPI spdlog::logger& GetLogger();
+	OEAPI void Log(Level level, std::u8string_view message);
 
-	template <FmtStr S, class... Args>
-	void Log(level::level_enum level, const S& fmt, Args&&... args)
+	template <class... Args>
+	void Log(Level level, std::u8string_view fmt, const Args&... args)
 	{
-		auto str = Format(fmt, std::forward<Args>(args)...);
-		GetLogger().log(level, reinterpret_cast<std::string&&>(str));
+		Log(level, Format(fmt, args...));
 	}
 	
-	template <FmtStr S, class... Args>
-	void Trace(const S& fmt, const Args&... args)
+	template <class... Args>
+	void Trace(std::u8string_view fmt, const Args&... args)
 	{
-		Log(level::trace, fmt, args...);
+		Log(Level::kTrace, fmt, args...);
 	}
 
-	template <FmtStr S, class... Args>
-	void Debug(const S& fmt, const Args&... args)
+	template <class... Args>
+	void Debug(std::u8string_view fmt, const Args&... args)
 	{
-		Log(level::debug, fmt, args...);
+		Log(Level::kDebug, fmt, args...);
 	}
 
-	template <FmtStr S, class... Args>
-	void Info(const S& fmt, const Args&... args)
+	template <class... Args>
+	void Info(std::u8string_view fmt, const Args&... args)
 	{
-		Log(level::info, fmt, args...);
+		Log(Level::kInfo, fmt, args...);
 	}
 
-	template <FmtStr S, class... Args>
-	void Warn(const S& fmt, const Args&... args)
+	template <class... Args>
+	void Warn(std::u8string_view fmt, const Args&... args)
 	{
-		Log(level::warn, fmt, args...);
+		Log(Level::kWarn, fmt, args...);
 	}
 
-	template <FmtStr S, class... Args>
-	void Error(const S& fmt, const Args&... args)
+	template <class... Args>
+	void Error(std::u8string_view fmt, const Args&... args)
 	{
-		Log(level::err, fmt, args...);
+		Log(Level::kErr, fmt, args...);
 	}
 
-	template <FmtStr S, class... Args>
-	void Critical(const S& fmt, const Args&... args)
+	template <class... Args>
+	void Critical(std::u8string_view fmt, const Args&... args)
 	{
-		Log(level::critical, fmt, args...);
+		Log(Level::kCritical, fmt, args...);
 	}
 
-	OEAPI void LogDelay(Duration delay, level::level_enum level, std::u8string_view msg);
+	OEAPI void LogDelay(Duration delay, Level level, std::u8string_view msg);
 
 	namespace detail
 	{
@@ -69,18 +63,18 @@ namespace oeng::log
 		public:
 			LogDelay() noexcept;
 			
-			void operator()(Duration delay, level::level_enum level, std::u8string_view msg) const;
+			void operator()(Duration delay, Level level, std::u8string_view msg) const;
 
 			template <class Rep, class Period, class... Args>
-			void operator()(time::duration<Rep, Period> delay, level::level_enum level, std::u8string_view msg) const
+			void operator()(time::duration<Rep, Period> delay, Level level, std::u8string_view msg) const
 			{
 				operator()(time::duration_cast<Duration>(delay), level, msg);
 			}
 
 			template <class Rep, class Period, class... Args>
-			void operator()(time::duration<Rep, Period> delay, level::level_enum level, std::u8string_view fmt, Args&&... args) const
+			void operator()(time::duration<Rep, Period> delay, Level level, std::u8string_view fmt, const Args&... args) const
 			{
-				operator()(time::duration_cast<Duration>(delay), level, Format(fmt, std::forward<Args>(args)...));
+				operator()(time::duration_cast<Duration>(delay), level, Format(fmt, args...));
 			}
 
 		private:
