db168777b922c07d3c8c58218c4647a6af9a77db
Othereum <seokjin.dev@gmail.com>
2020-07-12 17:29:47 +0900

    feat: Stat.hpp

---
+++ Source/Runtime/Core/Public/Stat.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include "Core.hpp"
+#include "Templates/Time.hpp"
+
+namespace oeng::detail
+{
+	class StopWatch
+	{
+	public:
+		explicit StopWatch(std::u8string_view name);
+		
+		void Start() noexcept
+		{
+			start_ = Clock::now();
+		}
+		
+		void Stop() noexcept
+		{
+			duration_ += Clock::now() - start_;
+			++count_;
+		}
+
+		[[nodiscard]] std::u8string_view Name() const noexcept
+		{
+			return name_;
+		}
+		
+		[[nodiscard]] Duration Average() const noexcept
+		{
+			if (count_ == 0) return {};
+			return duration_ / count_;
+		}
+
+	private:
+		std::u8string_view name_;
+		TimePoint start_{};
+		Duration duration_{};
+		uint64_t count_{};
+	};
+
+	class ScopeTimer
+	{
+	public:
+		OE_DELETE_CPMV(ScopeTimer);
+		
+		explicit ScopeTimer(StopWatch& watch) noexcept
+			:watch_{watch}
+		{
+			watch.Start();
+		}
+
+		~ScopeTimer()
+		{
+			watch_.Stop();
+		}
+
+	private:
+		StopWatch& watch_;
+	};
+}
+
+#define OE_STAT_SCOPE_TIMER(name) static ::oeng::detail::StopWatch _##name##StopWatch{u8## #name}; ::oeng::detail::ScopeTimer _##name##Timer{_##name##StopWatch}
