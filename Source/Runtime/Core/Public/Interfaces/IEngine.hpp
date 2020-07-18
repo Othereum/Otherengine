ba4f7027a3e1adbcb6a6ed0fbfb390ee7bbd6d32
Othereum <seokjin.dev@gmail.com>
2020-07-18 22:58:05 +0900

    feat: Memory pool in engine

--- Source/Runtime/Core/Public/Interfaces/IEngine.hpp
+++ Source/Runtime/Core/Public/Interfaces/IEngine.hpp
@@ -1,20 +1,21 @@
 #pragma once
+#include "Core.hpp"
 #include "JsonFwd.hpp"
-
-namespace oeng::engine
-{
-	class Engine;
-	class World;
-	class InputSystem;
-}
-
-namespace oeng::renderer
-{
-	class Renderer;
-}
 
 namespace oeng
 {
+	namespace engine
+	{
+		class Engine;
+		class World;
+		class InputSystem;
+	}
+
+	namespace renderer
+	{
+		class Renderer;
+	}
+
 	using namespace engine;
 	using namespace renderer;
 }
@@ -31,6 +32,24 @@ namespace oeng::core
 		[[nodiscard]] virtual World& GetWorld() noexcept = 0;
 		[[nodiscard]] virtual InputSystem& GetInputSystem() noexcept = 0;
 		[[nodiscard]] virtual Renderer& GetRenderer() noexcept = 0;
+		[[nodiscard]] virtual const char8_t* GetGameName() noexcept = 0;
+		
+		/**
+		 * \brief Get memory pool manager.
+		 * \return Reference to memory pool manager.
+		 * \note It should only be called from a game thread.
+		 */
+		[[nodiscard]] omem::MemoryPoolManager& GetMemPool() noexcept
+		{
+			assert(IsGameThread());
+			return GetMemPoolImpl();
+		}
+		
+		/**
+		 * \brief Check if the current thread is a game thread.
+		 * \return True if called from the game thread.
+		 */
+		[[nodiscard]] virtual bool IsGameThread() noexcept = 0;
 
 		/**
 		 * \brief Find config or create if not found.
@@ -46,6 +65,9 @@ namespace oeng::core
 		 * \return true if successful
 		 */
 		virtual bool SaveConfig(Name name) = 0;
+
+	protected:
+		[[nodiscard]] virtual omem::MemoryPoolManager& GetMemPoolImpl() noexcept = 0; 
 	};
 
 	extern CORE_API IEngine* const kIEngine;
