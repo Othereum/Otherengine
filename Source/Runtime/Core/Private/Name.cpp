aecd688e9f34878d9ad0ca55b0cd63a32ad88df3
Othereum <seokjin.dev@gmail.com>
2020-07-20 15:31:48 +0900

    feat: transparent find backward compatibility
    Clang 10 and GCC 10 does not support transparent find in C++20 not yet

--- Source/Runtime/Core/Private/Name.cpp
+++ Source/Runtime/Core/Private/Name.cpp
@@ -7,7 +7,7 @@
 namespace oeng::core
 {
 	Name::Name() noexcept
-		:sp{&*NAME_SET->find(std::u8string_view{})}
+		:sp{&*NAME_SET->find(String8{})}
 	{
 	}
 
@@ -20,11 +20,38 @@ namespace oeng::core
 		:sp{&*NAME_SET->insert(std::move(s)).first}
 	{
 	}
+	
+	template <class Container, class KeyEquivalent>
+	struct IsTransparent
+	{
+	private:
+	    template <class C, class = decltype(std::declval<C>().find(std::declval<KeyEquivalent>()))>
+	    static std::true_type Test(int);
+		
+	    template <class C>
+	    static std::false_type Test(...);
+
+	public:
+	    static constexpr bool value = decltype(Test<Container>(0))::value;
+	};
+
+	template <class Set>
+	auto Find(const Set& set, std::u8string_view s)
+	{
+		if constexpr (IsTransparent<Set, std::u8string_view>::value)
+		{
+			return set.find(s);
+		}
+		else
+		{
+			return set.find(s.data());
+		}
+	}
 
 	Name::Name(std::u8string_view s)
 	{
 		auto set = NAME_SET.Lock();
-		const auto found = set->find(s);
+		const auto found = Find(*set, s);
 		if (found != set->end())
 		{
 			sp = &*found;
