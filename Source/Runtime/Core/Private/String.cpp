d0d47295e74f3fb98e8228c145a0297743c79586
Othereum <seokjin.dev@gmail.com>
2020-07-20 15:35:11 +0900

    feat: Use pool-allocated string

--- Source/Runtime/Core/Private/String.cpp
+++ Source/Runtime/Core/Private/String.cpp
@@ -3,12 +3,12 @@
 
 namespace oeng::core
 {
-	std::u8string ToUtf8(std::u16string_view utf16)
+	String8 ToUtf8(std::u16string_view utf16)
 	{
 		auto& f = std::use_facet<std::codecvt<char16_t, char8_t, mbstate_t>>({});
 		
 		std::mbstate_t mb{};
-		std::u8string utf8(utf16.size() * f.max_length(), u8'\0');
+		String8 utf8(utf16.size() * f.max_length(), u8'\0');
 		
 		const char16_t* from_next;
 		char8_t* to_next;
@@ -20,12 +20,12 @@ namespace oeng::core
 		return utf8;
 	}
 
-	std::u16string ToUtf16(std::u8string_view utf8)
+	String16 ToUtf16(std::u8string_view utf8)
 	{
 		auto& f = std::use_facet<std::codecvt<char16_t, char8_t, mbstate_t>>({});
 		
 		std::mbstate_t mb{};
-		std::u16string utf16(utf8.size(), u'\0');
+		String16 utf16(utf8.size(), u'\0');
 		
 		const char8_t* from_next;
 		char16_t* to_next;
--- Source/Runtime/Core/Public/Templates/String.hpp
+++ Source/Runtime/Core/Public/Templates/String.hpp
@@ -126,6 +126,6 @@ namespace oeng::core
 		return reinterpret_cast<const char8_t(&)[N]>(s);
 	}
 
-	CORE_API std::u8string ToUtf8(std::u16string_view utf16);
-	CORE_API std::u16string ToUtf16(std::u8string_view utf8);
+	CORE_API String8 ToUtf8(std::u16string_view utf16);
+	CORE_API String16 ToUtf16(std::u8string_view utf8);
 }
