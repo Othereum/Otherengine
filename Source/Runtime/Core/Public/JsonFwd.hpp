bfc4e1804160ce09604cde34b6d4aceb39fba2f8
Othereum <seokjin.dev@gmail.com>
2020-07-21 17:18:56 +0900

    feat: Upgrade json to 3.8

--- Source/Runtime/Core/Public/JsonFwd.hpp
+++ Source/Runtime/Core/Public/JsonFwd.hpp
@@ -17,7 +17,8 @@ namespace nlohmann
 		class NumberUnsignedType,
 		class NumberFloatType,
 		template<typename U> class AllocatorType,
-		template<typename T, typename SFINAE> class JSONSerializer>
+		template<typename T, typename SFINAE> class JSONSerializer,
+		class BinaryType>
 	class basic_json;
 
 	namespace detail
@@ -29,6 +30,6 @@ namespace nlohmann
 
 namespace oeng::core
 {
-	using Json = nlohmann::basic_json<TreeMap, DyArr, std::string, bool, int64_t, uint64_t, Float, PoolAllocator, nlohmann::adl_serializer>;
+	using Json = nlohmann::basic_json<TreeMap, DyArr, std::string, bool, int64_t, uint64_t, Float, PoolAllocator, nlohmann::adl_serializer, DyArr<std::uint8_t>>;
 	using JsonType = nlohmann::detail::value_t;
 }
