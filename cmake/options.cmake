bff9aef361b2e9bd4112c1eee23e067cd243717d
Othereum <seokjin.dev@gmail.com>
2020-07-22 23:32:50 +0900

    chore: Exclude build docs from cmake

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,4 +1,3 @@
-option(OE_BUILD_DOCS "Whether to build documents with doxygen")
 option(OE_USE_AVX2 "Use AVX2?" ON)
 
 option(OE_DEPS_FORCE_INTERNAL "Force dependencies to be downloaded and built directly.")
