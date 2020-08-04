66a2597243538ae526598eb994a38e5497b54561
Othereum <seokjin.dev@gmail.com>
2020-08-04 14:12:17 +0900

    chore: Default version

--- GetCMake.sh
+++ GetCMake.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+[[ $1 ]] && ver=$1 || ver=3.18.1
+
 if [[ ! -f cmake-install/bin/cmake ]]; then
     mkdir -p Downloads
     cd Downloads
 
-    wget -nc https://github.com/Kitware/CMake/releases/download/v$1/cmake-$1-Linux-x86_64.tar.gz
-    tar -xf cmake-$1-Linux-x86_64.tar.gz
-    mv cmake-$1-Linux-x86_64 ../cmake-install
+    wget -nc https://github.com/Kitware/CMake/releases/download/v$ver/cmake-$ver-Linux-x86_64.tar.gz
+    tar -xf cmake-$ver-Linux-x86_64.tar.gz
+    mv cmake-$ver-Linux-x86_64 ../cmake-install
 fi
--- GetFMOD.sh
+++ GetFMOD.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -e
 
+[[ $3 ]] && ver=$3 || ver=20010
+
 if [[ ! -d Source/ThirdParty/fmod ]]; then
     mkdir -p Downloads
     cd Downloads
 
-    if [[ ! -f fmodstudioapi$3linux.tar.gz ]]; then
+    if [[ ! -f fmodstudioapi${ver}linux.tar.gz ]]; then
         TOKEN=$(curl -X POST -u $1:$2 https://www.fmod.com/api-login | jq -r '.token')
-        URL=$(curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" https://www.fmod.com/api-get-download-link\?path\=files/fmodstudio/api/Linux/\&filename\=fmodstudioapi$3linux.tar.gz\&user\=$1 | jq -r '.url')
-        wget -O fmodstudioapi$3linux.tar.gz $URL
+        URL=$(curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" https://www.fmod.com/api-get-download-link\?path\=files/fmodstudio/api/Linux/\&filename\=fmodstudioapi${ver}linux.tar.gz\&user\=$1 | jq -r '.url')
+        wget -O fmodstudioapi${ver}linux.tar.gz $URL
     fi
 
-    tar -xf fmodstudioapi$3linux.tar.gz
-    mv fmodstudioapi$3linux ../Source/ThirdParty/fmod
+    tar -xf fmodstudioapi${ver}linux.tar.gz
+    mv fmodstudioapi${ver}linux ../Source/ThirdParty/fmod
 fi
