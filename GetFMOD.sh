9e0a1a980bd340a9b41a8be434e29c61f2ba4e72
Othereum <seokjin.dev@gmail.com>
2020-08-04 11:13:27 +0900

    chore: ...

--- GetFMOD.sh
+++ GetFMOD.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-if [[ ! -d Source/ThirdParty/fmod ]]
+if [[ ! -d Source/ThirdParty/fmod ]]; then
     mkdir -p Downloads
     cd Downloads
 
-    if [[ ! -f fmodstudioapi$3linux.tar.gz ]]
+    if [[ ! -f fmodstudioapi$3linux.tar.gz ]]; then
         TOKEN=$(curl -X POST -u $1:$2 https://www.fmod.com/api-login | jq -r '.token')
         URL=$(curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" https://www.fmod.com/api-get-download-link\?path\=files/fmodstudio/api/Linux/\&filename\=fmodstudioapi$3linux.tar.gz\&user\=$1 | jq -r '.url')
         wget -O fmodstudioapi$3linux.tar.gz $URL
