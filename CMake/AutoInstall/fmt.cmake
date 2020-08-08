4deb736873b6ceb7747a1cdc698685c05be245a3
Othereum <seokjin.dev@gmail.com>
2020-08-08 13:37:17 +0900

    chore: Update fmt to 7.0.3

--- CMake/AutoInstall/fmt.cmake
+++ CMake/AutoInstall/fmt.cmake
@@ -4,7 +4,7 @@ project(@name@-download NONE)
 include(ExternalProject)
 ExternalProject_Add(@name@-download
 	GIT_REPOSITORY		https://github.com/fmtlib/fmt
-	GIT_TAG				7.0.2
+	GIT_TAG				7.0.3
 	GIT_SHALLOW			TRUE
 	GIT_PROGRESS		TRUE
 	SOURCE_DIR			"@src_dir@"
