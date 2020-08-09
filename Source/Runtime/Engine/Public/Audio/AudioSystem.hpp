b553fae5a719fe99c5e59756b10b5fa63331a576
Othereum <seokjin.dev@gmail.com>
2020-08-09 10:16:14 +0900

    refactor: Remove

--- Source/Runtime/Engine/Public/Audio/AudioSystem.hpp
+++ Source/Runtime/Engine/Public/Audio/AudioSystem.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "SoundEvent.hpp"
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 namespace FMOD
@@ -15,6 +16,10 @@ namespace FMOD
 
 namespace oeng::engine
 {
+	class AudioSystemError : public std::runtime_error {};
+	class FModError : public AudioSystemError {};
+	class BankAlreadyLoaded : public AudioSystemError {};
+	
 	class ENGINE_API AudioSystem
 	{
 	public:
@@ -26,22 +31,31 @@ namespace oeng::engine
 		void Update(Float delta_seconds) const;
 
 		/**
-		 * Load the bank and its sample data and events.
+		 * @brief Load the bank and its sample data and events.
+		 * @param path Bank file path. Must be valid.
 		 * @return False if already loaded. True if successful.
 		 * @throw std::runtime_error If failed to load the bank.
 		 */
 		bool LoadBank(Path path);
 
 		/**
-		 * Unload the bank and its sample data and events.
+		 * @brief Unload the bank and its sample data and events.
+		 * @param path Bank file path.
 		 * @return False if already unloaded. True if successful.
 		 */
 		bool UnloadBank(Path path);
 
 		/**
-		 * Unload all banks, sample data and events.
+		 * @brief Unload all banks, sample data and events.
 		 */
 		void UnloadAllBanks();
+
+		/**
+		 * @brief Create an event instance and start playback.
+		 * @param name Event name. Must be valid.
+		 * @return Sound event.
+		*/
+		SoundEvent PlayEvent(Name name);
 
 	private:
 		void UnloadBank(FMOD::Studio::Bank& bank);
@@ -51,5 +65,8 @@ namespace oeng::engine
 
 		HashMap<Path, FMOD::Studio::Bank*> banks_;
 		HashMap<Name, FMOD::Studio::EventDescription*> events_;
+
+		unsigned next_id_ = 0;
+		HashMap<unsigned, FMOD::Studio::EventInstance*> event_instances_;
 	};
 }
