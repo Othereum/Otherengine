530b042c8820a9439f84acccc361910df955001a
Othereum <seokjin.dev@gmail.com>
2020-08-12 23:34:09 +0900

    refactor: Shader

--- Source/Runtime/Renderer/Private/Shader.cpp
+++ Source/Runtime/Renderer/Private/Shader.cpp
@@ -1,161 +1,233 @@
 #include "Shader.hpp"
+#include "Renderer.hpp"
 #include <fstream>
 #include <GL/glew.h>
-#include "Renderer.hpp"
 
 namespace oeng::renderer
 {
-	std::string ReadFile(const std::filesystem::path& path)
-	{
-		std::ifstream file{ path, std::ios_base::in | std::ios_base::ate};
-		if (!file.is_open()) Throw<std::ios_base::failure>(u8"Cannot read file. File not found: {}"sv, path.string<char8_t>(PoolAllocator<char8_t>{}));
+std::string ReadFile(const fs::path& path)
+{
+    std::ifstream file{path, std::ios_base::in | std::ios_base::ate};
 
-		std::string code(file.tellg(), '\0');
-		file.seekg(0);
-		file.read(code.data(), code.size());
-		return code;
-	}
-	
-	static void CheckShader(unsigned shader)
-	{
-		auto is_valid = 0;
-		glGetShaderiv(shader, GL_COMPILE_STATUS, &is_valid);
-		if (!is_valid)
-		{
-			auto len = 0;
-			glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
-			
-			std::string log(len, '\0');
-			glGetShaderInfoLog(shader, len, nullptr, log.data());
-			
-			throw std::runtime_error{log};
-		}
-	}
+    if (!file.is_open())
+        throw std::ios_base::failure{fmt::format("Cannot read file. File not found: {}"sv, path.string())};
 
-	static void CheckProgram(unsigned program)
-	{
-		auto is_valid = 0;
-		glGetProgramiv(program, GL_LINK_STATUS, &is_valid);
-		if (!is_valid)
-		{
-			auto len = 0;
-			glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len);
-			
-			std::string log(len, '\0');
-			glGetProgramInfoLog(program, len, nullptr, log.data());
-			
-			throw std::runtime_error{log};
-		}
-	}
-	
-	static unsigned Compile(const std::filesystem::path& file, unsigned type)
-	{
-		const auto code = ReadFile(file);
-		const auto* const c_str = code.c_str();
-		const auto shader = glCreateShader(type);
-		glShaderSource(shader, 1, &c_str, nullptr);
-		glCompileShader(shader);
-		CheckShader(shader);
-		return shader;
-	}
+    std::string code(file.tellg(), '\0');
+    file.seekg(0);
+    file.read(code.data(), code.size());
+    return code;
+}
 
-	static auto Ext(std::filesystem::path path, const char8_t* ext)
-	{
-		return path += ext;
-	}
+template <class GetParam, class GetInfoLog>
+static void CheckValid(unsigned object, GetParam get_param, unsigned status_param_name, GetInfoLog get_info_log)
+{
+    int is_valid;
+    get_param(object, status_param_name, &is_valid);
 
-	Shader::Shader(Path path)
-		:Asset{path},
-		vert_shader_{Compile(Ext(path, u8".vert"), GL_VERTEX_SHADER)},
-		frag_shader_{Compile(Ext(path, u8".frag"), GL_FRAGMENT_SHADER)},
-		shader_program_{glCreateProgram()}
-	{
-		glAttachShader(shader_program_, vert_shader_);
-		glAttachShader(shader_program_, frag_shader_);
-		glLinkProgram(shader_program_);
-		CheckProgram(shader_program_);
-		Activate();
-	}
+    if (is_valid == GL_FALSE)
+    {
+        int buf_sz;
+        get_param(object, GL_INFO_LOG_LENGTH, &buf_sz);
 
-	Shader::~Shader()
-	{
-		glDeleteProgram(shader_program_);
-		glDeleteShader(vert_shader_);
-		glDeleteShader(frag_shader_);
-	}
+        int len;
+        std::string log(buf_sz - 1, '\0');
+        get_info_log(object, buf_sz, &len, log.data());
 
-	void Shader::Activate() const
-	{
-		glUseProgram(shader_program_);
-	}
+        throw ShaderCompileError{log};
+    }
+}
 
-	template <class... Args, std::invocable<int, Args...> Fn>
-	static bool GlUniform(Fn fn, int location, Args... args) noexcept
-	{
-		fn(location, args...);
-		return glGetError() == GL_NO_ERROR;
-	}
+static void CheckShader(unsigned shader)
+{
+    CheckValid(shader, glGetShaderiv, GL_COMPILE_STATUS, glGetShaderInfoLog);
+}
 
-	template <class Fn, size_t Row, size_t Col>
-	static bool GlUniform(Fn fn, int l, const Matrix<Float, Row, Col>& v) noexcept
-	{
-		return GlUniform(fn, l, 1, true, v.AsFlatArr());
-	}
-	
-	static bool GlUniform(int l, const Matrix<Float, 2, 2>& v) noexcept { return GlUniform(glUniformMatrix2fv, l, v); }
-	static bool GlUniform(int l, const Matrix<Float, 2, 3>& v) noexcept { return GlUniform(glUniformMatrix2x3fv, l, v); }
-	static bool GlUniform(int l, const Matrix<Float, 2, 4>& v) noexcept { return GlUniform(glUniformMatrix2x4fv, l, v); }
-	static bool GlUniform(int l, const Matrix<Float, 3, 2>& v) noexcept { return GlUniform(glUniformMatrix3x2fv, l, v); }
-	static bool GlUniform(int l, const Matrix<Float, 3, 3>& v) noexcept { return GlUniform(glUniformMatrix3fv, l, v); }
-	static bool GlUniform(int l, const Matrix<Float, 3, 4>& v) noexcept { return GlUniform(glUniformMatrix3x4fv, l, v); }
-	static bool GlUniform(int l, const Matrix<Float, 4, 2>& v) noexcept { return GlUniform(glUniformMatrix4x2fv, l, v); }
-	static bool GlUniform(int l, const Matrix<Float, 4, 3>& v) noexcept { return GlUniform(glUniformMatrix4x3fv, l, v); }
-	static bool GlUniform(int l, const Matrix<Float, 4, 4>& v) noexcept { return GlUniform(glUniformMatrix4fv, l, v); }
+static void CheckProgram(unsigned program)
+{
+    CheckValid(program, glGetProgramiv, GL_LINK_STATUS, glGetProgramInfoLog);
+}
 
-	static bool GlUniform(int l, Vec2 v) noexcept { return GlUniform(glUniform2f, l, v[0], v[1]); }
-	static bool GlUniform(int l, Vec3 v) noexcept { return GlUniform(glUniform3f, l, v[0], v[1], v[2]); }
-	static bool GlUniform(int l, Vec4 v) noexcept { return GlUniform(glUniform4f, l, v[0], v[1], v[2], v[3]); }
-	static bool GlUniform(int l, Vector<int, 2> v) noexcept { return GlUniform(glUniform2i, l, v[0], v[1]); }
-	static bool GlUniform(int l, Vector<int, 3> v) noexcept { return GlUniform(glUniform3i, l, v[0], v[1], v[2]); }
-	static bool GlUniform(int l, Vector<int, 4> v) noexcept { return GlUniform(glUniform4i, l, v[0], v[1], v[2], v[3]); }
+static unsigned Compile(const fs::path& file, unsigned type)
+{
+    const auto code = ReadFile(file);
+    const auto* const c_str = code.c_str();
 
-	static bool GlUniform(int l, float v) noexcept { return GlUniform(glUniform1f, l, v); }
-	static bool GlUniform(int l, int v) noexcept { return GlUniform(glUniform1i, l, v); }
+    const auto shader = glCreateShader(type);
+    glShaderSource(shader, 1, &c_str, nullptr);
+    glCompileShader(shader);
 
-	bool Shader::SetUniform(int location, const Uniform& value)
-	{
-		SCOPE_COUNTER(SetUniform);
+    CheckShader(shader);
+    return shader;
+}
 
-		if (location == invalid_uniform)
-		{
-			OE_LOG(kRenderer, kWarn, u8"Attempted to set uniform with invalid location -1"sv);
-			return false;
-		}
+static fs::path Ext(fs::path path, std::u8string_view ext)
+{
+    path += ext;
+    return path;
+}
 
-		const auto cache = uniform_cache_.find(location);
-		if (cache != uniform_cache_.end())
-		{
-			auto equals = []<class T1, class T2>(const T1& a, const T2& b)
-			{
-				if constexpr (!std::is_same_v<T1, T2>) { return false; }
-				else { return IsNearlyEqual(a, b); }
-			};
-			if (std::visit(equals, cache->second, value)) return true;
-		}
-		
-		SCOPE_COUNTER(RealSetUniform);
-		const auto success = std::visit([&](const auto& v) { return GlUniform(location, v); }, value);
-		if (success) uniform_cache_.insert_or_assign(cache, location, value);
-		return success;
-	}
+Shader::Shader(Path path)
+    : Asset{path},
+      vert_shader_{Compile(Ext(path, u8".vert"sv), GL_VERTEX_SHADER)},
+      frag_shader_{Compile(Ext(path, u8".frag"sv), GL_FRAGMENT_SHADER)},
+      shader_program_{glCreateProgram()}
+{
+    glAttachShader(shader_program_, vert_shader_);
+    glAttachShader(shader_program_, frag_shader_);
+    glLinkProgram(shader_program_);
+    CheckProgram(shader_program_);
+    Activate();
+}
 
-	int Shader::GetUniformLocation(Name name) noexcept
-	{
-		if (const auto found = loc_cache_.find(name); found != loc_cache_.end())
-			return found->second;
+Shader::~Shader()
+{
+    glDeleteProgram(shader_program_);
+    glDeleteShader(vert_shader_);
+    glDeleteShader(frag_shader_);
+}
 
-		const auto loc = glGetUniformLocation(shader_program_, AsString(name->c_str()));
-		if (loc != invalid_uniform) loc_cache_.try_emplace(name, loc);
-		return loc;
-	}
+void Shader::Activate() const
+{
+    glUseProgram(shader_program_);
+}
+
+static void GlUniform(int l, const Matrix<Float, 2, 2>& v) noexcept
+{
+    glUniformMatrix2fv(l, 1, true, v.AsFlatArr());
+}
+
+static void GlUniform(int l, const Matrix<Float, 2, 3>& v) noexcept
+{
+    glUniformMatrix2x3fv(l, 1, true, v.AsFlatArr());
+}
+
+static void GlUniform(int l, const Matrix<Float, 2, 4>& v) noexcept
+{
+    glUniformMatrix2x4fv(l, 1, true, v.AsFlatArr());
+}
+
+static void GlUniform(int l, const Matrix<Float, 3, 2>& v) noexcept
+{
+    glUniformMatrix3x2fv(l, 1, true, v.AsFlatArr());
+}
+
+static void GlUniform(int l, const Matrix<Float, 3, 3>& v) noexcept
+{
+    glUniformMatrix3fv(l, 1, true, v.AsFlatArr());
+}
+
+static void GlUniform(int l, const Matrix<Float, 3, 4>& v) noexcept
+{
+    glUniformMatrix3x4fv(l, 1, true, v.AsFlatArr());
+}
+
+static void GlUniform(int l, const Matrix<Float, 4, 2>& v) noexcept
+{
+    glUniformMatrix4x2fv(l, 1, true, v.AsFlatArr());
+}
+
+static void GlUniform(int l, const Matrix<Float, 4, 3>& v) noexcept
+{
+    glUniformMatrix4x3fv(l, 1, true, v.AsFlatArr());
+}
+
+static void GlUniform(int l, const Matrix<Float, 4, 4>& v) noexcept
+{
+    glUniformMatrix4fv(l, 1, true, v.AsFlatArr());
+}
+
+static void GlUniform(int l, Vec2 v) noexcept
+{
+    glUniform2f(l, v[0], v[1]);
+}
+
+static void GlUniform(int l, Vec3 v) noexcept
+{
+    glUniform3f(l, v[0], v[1], v[2]);
+}
+
+static void GlUniform(int l, Vec4 v) noexcept
+{
+    glUniform4f(l, v[0], v[1], v[2], v[3]);
+}
+
+static void GlUniform(int l, Vector<int, 2> v) noexcept
+{
+    glUniform2i(l, v[0], v[1]);
+}
+
+static void GlUniform(int l, Vector<int, 3> v) noexcept
+{
+    glUniform3i(l, v[0], v[1], v[2]);
+}
+
+static void GlUniform(int l, Vector<int, 4> v) noexcept
+{
+    glUniform4i(l, v[0], v[1], v[2], v[3]);
+}
+
+static void GlUniform(int l, float v) noexcept
+{
+    glUniform1f(l, v);
+}
+
+static void GlUniform(int l, int v) noexcept
+{
+    glUniform1i(l, v);
+}
+
+bool Shader::SetUniform(int location, const Uniform& value)
+{
+    SCOPE_COUNTER(SetUniform);
+
+    if (location == invalid_uniform)
+    {
+        OE_LOG(kRenderer, kWarn, u8"Attempted to set uniform with invalid location -1"sv);
+        return false;
+    }
+
+    const auto cache = uniform_cache_.find(location);
+    if (cache != uniform_cache_.end())
+    {
+        auto equals = []<class T1, class T2>(const T1& a, const T2& b)
+        {
+            if constexpr (!std::is_same_v<T1, T2>)
+            {
+                return false;
+            }
+            else
+            {
+                return IsNearlyEqual(a, b);
+            }
+        };
+
+        if (std::visit(equals, cache->second, value))
+            return true;
+    }
+
+    std::visit([&](const auto& v)
+    {
+        SCOPE_COUNTER(RealSetUniform);
+        return GlUniform(location, v);
+    }, value);
+
+    if (glGetError() != GL_NO_ERROR)
+        return false;
+
+    uniform_cache_.insert_or_assign(cache, location, value);
+    return true;
+}
+
+int Shader::GetUniformLocation(Name name) noexcept
+{
+    if (const auto found = loc_cache_.find(name); found != loc_cache_.end())
+        return found->second;
+
+    const auto loc = glGetUniformLocation(shader_program_, AsString(name->c_str()));
+
+    if (loc != invalid_uniform)
+        loc_cache_.try_emplace(name, loc);
+
+    return loc;
+}
 }
--- Source/Runtime/Renderer/Public/Shader.hpp
+++ Source/Runtime/Renderer/Public/Shader.hpp
@@ -1,126 +1,151 @@
 #pragma once
-#include <compare>
-#include "Asset.hpp"
 #include "Uniform.hpp"
-#include "Templates/std::unordered_map.hpp"
 
 namespace oeng::renderer
 {
-	class RENDERER_API Shader : public Asset
-	{
-	public:
-		static constexpr int invalid_uniform = -1;
+/**
+ * Exception class indicating shader compilation error. `what()` returns the compilation error log.
+ */
+class ShaderCompileError : public std::runtime_error
+{
+    explicit ShaderCompileError(const std::string& message)
+        : std::runtime_error{message}
+    {
+    }
 
-		Shader() = default;
-		explicit Shader(Path path);
-		
-		void Load(Path path)
-		{
-			Shader{path}.swap(*this);
-		}
-		
-		~Shader();
-		
-		Shader(Shader&& r) noexcept
-			:Asset{std::move(r)}, vert_shader_{r.vert_shader_}, frag_shader_{r.frag_shader_},
-			shader_program_{r.shader_program_}, loc_cache_{std::move(r.loc_cache_)},
-			uniform_cache_{std::move(r.uniform_cache_)}
-		{
-			r.vert_shader_ = 0;
-			r.frag_shader_ = 0;
-			r.shader_program_ = 0;
-		}
-		
-		Shader& operator=(Shader&& r) noexcept
-		{
-			Shader{std::move(r)}.swap(*this);
-			return *this;
-		}
+    explicit ShaderCompileError(const char* message)
+        : std::runtime_error{message}
+    {
+    }
+};
 
-		Shader(const Shader&) = delete;
-		Shader& operator=(const Shader&) = delete;
+class RENDERER_API Shader : public Asset
+{
+public:
+    static constexpr int invalid_uniform = -1;
 
-		std::strong_ordering operator<=>(const Shader& r) const noexcept
-		{
-			return shader_program_ <=> r.shader_program_;
-		}
+    Shader() = default;
+    ~Shader();
 
-		void Activate() const;
-		
-		/**
-		 * Try to set uniform variable of this shader with given name.
-		 * If the name is not valid, it returns false quietly.
-		 * Otherwise, error will be logged if occurs.
-		 * @param name The name of the uniform variable.
-		 * @param value The new value to be set.
-		 * @return True if successful.
-		 */
-		bool TryUniform(Name name, const Uniform& value)
-		{
-			const auto location = GetUniformLocation(name);
-			if (location == invalid_uniform) return false;
-			return SetUniform(location, value);
-		}
+    /**
+     * Load shader from given path.
+     * @param path Shader path. For example, "../Shaders/Phong" loads Phong.vert and Phong.frag.
+     * @throw std::ios_base::failure If failed to open file.
+     * @throw ShaderCompileError If compilation failed.
+     */
+    explicit Shader(Path path);
 
-		/**
-		 * Set uniform variable of this shader with given name.
-		 * An error will be logged if occurs.
-		 * @param name The name of the uniform variable. Must be valid.
-		 * @param value The new value to be set.
-		 * @return True if successful.
-		 */
-		bool SetUniform(Name name, const Uniform& value)
-		{
-			return SetUniform(GetUniformLocation(name), value);
-		}
+    Shader(Shader&& r) noexcept
+        : Asset{std::move(r)}, vert_shader_{r.vert_shader_}, frag_shader_{r.frag_shader_},
+          shader_program_{r.shader_program_}, loc_cache_{std::move(r.loc_cache_)},
+          uniform_cache_{std::move(r.uniform_cache_)}
+    {
+        r.vert_shader_ = 0;
+        r.frag_shader_ = 0;
+        r.shader_program_ = 0;
+    }
 
-		/**
-		 * Set uniform variable of this shader with given location.
-		 * An error will be logged if occurs.
-		 * @param location The location of the uniform variable. Must be valid.
-		 * @param value The new value to be set.
-		 * @return True if successful.
-		 */
-		bool SetUniform(int location, const Uniform& value);
-		
-		/**
-		 * Get location of the uniform variable
-		 * @param name The name of the uniform
-		 * @return Location of the uniform or invalid_uniform if name is invalid
-		 */
-		[[nodiscard]] int GetUniformLocation(Name name) noexcept;
+    void Load(Path path)
+    {
+        Shader{path}.swap(*this);
+    }
 
-		void swap(Shader& r) noexcept
-		{
-			Asset::swap(r);
-			using std::swap;
-			swap(vert_shader_, r.vert_shader_);
-			swap(frag_shader_, r.frag_shader_);
-			swap(shader_program_, r.shader_program_);
-			swap(loc_cache_, r.loc_cache_);
-			swap(uniform_cache_, r.uniform_cache_);
-		}
+    Shader& operator=(Shader&& r) noexcept
+    {
+        Shader{std::move(r)}.swap(*this);
+        return *this;
+    }
 
-	private:
-		friend std::hash<Shader>;
+    Shader(const Shader&) = delete;
+    Shader& operator=(const Shader&) = delete;
 
-		unsigned vert_shader_ = 0;
-		unsigned frag_shader_ = 0;
-		unsigned shader_program_ = 0;
-		std::unordered_map<Name, int> loc_cache_;
-		std::unordered_map<int, Uniform> uniform_cache_;
-	};
+    std::strong_ordering operator<=>(const Shader& r) const noexcept
+    {
+        return shader_program_ <=> r.shader_program_;
+    }
 
-	inline void swap(Shader& a, Shader& b) noexcept { a.swap(b); }
+    void Activate() const;
+
+    /**
+     * Try to set uniform variable of this shader with given name.
+     * If the name is not valid, it returns false quietly.
+     * Otherwise, error will be logged if occurs.
+     * @param name The name of the uniform variable.
+     * @param value The new value to be set.
+     * @return True if successful.
+     */
+    bool TryUniform(Name name, const Uniform& value)
+    {
+        const auto location = GetUniformLocation(name);
+
+        if (location == invalid_uniform)
+            return false;
+
+        return SetUniform(location, value);
+    }
+
+    /**
+     * Set uniform variable of this shader with given name.
+     * An error will be logged if occurs.
+     * @param name The name of the uniform variable. Must be valid.
+     * @param value The new value to be set.
+     * @return True if successful.
+     */
+    bool SetUniform(Name name, const Uniform& value)
+    {
+        return SetUniform(GetUniformLocation(name), value);
+    }
+
+    /**
+     * Set uniform variable of this shader with given location.
+     * An error will be logged if occurs.
+     * @param location The location of the uniform variable. Must be valid.
+     * @param value The new value to be set.
+     * @return True if successful.
+     */
+    bool SetUniform(int location, const Uniform& value);
+
+    /**
+     * Get location of the uniform variable
+     * @param name The name of the uniform
+     * @return Location of the uniform or invalid_uniform if name is invalid
+     */
+    [[nodiscard]] int GetUniformLocation(Name name) noexcept;
+
+    void swap(Shader& r) noexcept
+    {
+        Asset::swap(r);
+        using std::swap;
+        swap(vert_shader_, r.vert_shader_);
+        swap(frag_shader_, r.frag_shader_);
+        swap(shader_program_, r.shader_program_);
+        swap(loc_cache_, r.loc_cache_);
+        swap(uniform_cache_, r.uniform_cache_);
+    }
+
+private:
+    friend std::hash<Shader>;
+
+    unsigned vert_shader_ = 0;
+    unsigned frag_shader_ = 0;
+    unsigned shader_program_ = 0;
+    std::unordered_map<Name, int> loc_cache_;
+    std::unordered_map<int, Uniform> uniform_cache_;
+};
+
+inline void swap(Shader& a, Shader& b) noexcept
+{
+    a.swap(b);
+}
 }
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 template <>
 struct std::hash<oeng::renderer::Shader>
 {
-	size_t operator()(const oeng::renderer::Shader& key) const noexcept
-	{
-		return size_t(key.shader_program_);
-	}
+    size_t operator()(const oeng::renderer::Shader& key) const noexcept
+    {
+        return size_t(key.shader_program_);
+    }
 };
 #endif
