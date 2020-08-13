2096c11e10323de02e0c5bf7efa2f157bd6d24a2
Othereum <seokjin.dev@gmail.com>
2020-08-13 11:52:12 +0900

    feat: New class `Resource`

---
+++ Source/Runtime/Core/Public/Templates/Resource.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include "CompPair.hpp"
+
+namespace oeng::core
+{
+/**
+ * RAII resource handler
+ */
+template <std::regular T, std::invocable<T> Deleter>
+class Resource
+{
+public:
+    Resource() = default;
+
+    explicit Resource(T id)
+        : storage_{ZeroThen{}, std::move(id)}
+    {
+    }
+
+    Resource(T id, Deleter deleter)
+        : storage_{OneThen{}, std::move(deleter), std::move(id)}
+    {
+    }
+
+    ~Resource()
+    {
+        storage_.first()(storage_.second());
+    }
+
+    Resource(Resource&& r) noexcept
+        : storage_{std::move(r)}
+    {
+        r = {};
+    }
+
+    Resource& operator=(Resource&& r) noexcept
+    {
+        Resource{std::move(r)}.swap(*this);
+        return *this;
+    }
+
+    Resource(const Resource&) = delete;
+    Resource& operator=(const Resource&) = delete;
+
+    T& operator*() noexcept
+    {
+        return storage_.second();
+    }
+
+    const T& operator*() const noexcept
+    {
+        return storage_.second();
+    }
+
+    T* operator->() noexcept
+    {
+        return &storage_.second();
+    }
+
+    const T* operator->() const noexcept
+    {
+        return &storage_.second();
+    }
+
+    void swap(Resource& r) noexcept
+    {
+        using std::swap;
+        swap(storage_, r.storage_);
+    }
+
+private:
+    CompPair<Deleter, T> storage_;
+};
+
+template <std::regular T, std::invocable<T> Deleter>
+void swap(Resource<T, Deleter>& a, Resource<T, Deleter>& b) noexcept
+{
+    a.swap(b);
+}
+}
