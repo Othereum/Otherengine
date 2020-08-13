07025526566125c05f7707b518865ef61d65d498
Othereum <seokjin.dev@gmail.com>
2020-08-13 11:52:43 +0900

    style

--- Source/Runtime/Core/Public/Templates/CompPair.hpp
+++ Source/Runtime/Core/Public/Templates/CompPair.hpp
@@ -2,77 +2,107 @@
 
 namespace oeng::core
 {
-	/**
-	 * Tag type for value-initializing first, constructing second from remaining args
-	 */
-	struct ZeroThen
-	{
-		explicit ZeroThen() = default;
-	};
+/**
+ * Tag type for value-initializing first, constructing second from remaining args
+ */
+struct ZeroThen
+{
+    explicit ZeroThen() = default;
+};
 
-	/**
-	 * Tag type for constructing first from one arg, constructing second from remaining args
-	 */
-	struct OneThen
-	{
-		explicit OneThen() = default;
-	};
-	
-	/**
-	 * Compressed pair. If the first type is derivative and empty, it is compressed.
-	 */
-	template <class T1, class T2, bool = std::is_empty_v<T1> && !std::is_final_v<T1>>
-	class CompPair final : T1
-	{
-	public:
-		template <class... Second>
-		constexpr explicit CompPair(ZeroThen, Second&&... second) noexcept(
-			std::conjunction_v<std::is_nothrow_default_constructible<T1>, std::is_nothrow_constructible<T2, Second...>>)
-			: T1{}, second_{std::forward<Second>(second)...}
-		{
-		}
+/**
+ * Tag type for constructing first from one arg, constructing second from remaining args
+ */
+struct OneThen
+{
+    explicit OneThen() = default;
+};
 
-		template <class First, class... Second>
-		constexpr CompPair(OneThen, First&& first, Second&&... second) noexcept(
-			std::conjunction_v<std::is_nothrow_constructible<T1, First>, std::is_nothrow_constructible<T2, Second...>>)
-			: T1{std::forward<First>(first)}, second_{std::forward<Second>(second)...}
-		{
-		}
+/**
+ * Compressed pair. If the first type is derivative and empty, it is compressed.
+ */
+template <class T1, class T2, bool = std::is_empty_v<T1> && !std::is_final_v<T1>>
+class CompPair final : T1
+{
+public:
+    template <class... Second>
+    constexpr explicit CompPair(ZeroThen, Second&&... second) noexcept(
+        std::conjunction_v<std::is_nothrow_default_constructible<T1>, std::is_nothrow_constructible<T2, Second...>>)
+        : T1{}, second_{std::forward<Second>(second)...}
+    {
+    }
 
-		[[nodiscard]] constexpr T1& first() noexcept { return *this; }
-		[[nodiscard]] constexpr T2& second() noexcept { return second_; }
-		[[nodiscard]] constexpr const T1& first() const noexcept { return *this; }
-		[[nodiscard]] constexpr const T2& second() const noexcept { return second_; }
+    template <class First, class... Second>
+    constexpr CompPair(OneThen, First&& first, Second&&... second) noexcept(
+        std::conjunction_v<std::is_nothrow_constructible<T1, First>, std::is_nothrow_constructible<T2, Second...>>)
+        : T1{std::forward<First>(first)}, second_{std::forward<Second>(second)...}
+    {
+    }
 
-	private:
-		T2 second_;
-	};
+    [[nodiscard]] constexpr T1& first() noexcept
+    {
+        return *this;
+    }
 
-	template <class T1, class T2>
-	class CompPair<T1, T2, false> final
-	{
-	public:
-		template <class... Second>
-		constexpr explicit CompPair(ZeroThen, Second&&... second) noexcept(
-			std::conjunction_v<std::is_nothrow_default_constructible<T1>, std::is_nothrow_constructible<T2, Second...>>)
-			: first_{}, second_{std::forward<Second>(second)...}
-		{
-		}
+    [[nodiscard]] constexpr T2& second() noexcept
+    {
+        return second_;
+    }
 
-		template <class First, class... Second>
-		constexpr CompPair(OneThen, First&& first, Second&&... second) noexcept(
-			std::conjunction_v<std::is_nothrow_constructible<T1, First>, std::is_nothrow_constructible<T2, Second...>>)
-			: first_{std::forward<First>(first)}, second_{std::forward<Second>(second)...}
-		{
-		}
+    [[nodiscard]] constexpr const T1& first() const noexcept
+    {
+        return *this;
+    }
 
-		[[nodiscard]] constexpr T1& first() noexcept { return first_; }
-		[[nodiscard]] constexpr T2& second() noexcept { return second_; }
-		[[nodiscard]] constexpr const T1& first() const noexcept { return first_; }
-		[[nodiscard]] constexpr const T2& second() const noexcept { return second_; }
+    [[nodiscard]] constexpr const T2& second() const noexcept
+    {
+        return second_;
+    }
 
-	private:
-		T1 first_;
-		T2 second_;
-	};
+private:
+    T2 second_;
+};
+
+template <class T1, class T2>
+class CompPair<T1, T2, false> final
+{
+public:
+    template <class... Second>
+    constexpr explicit CompPair(ZeroThen, Second&&... second) noexcept(
+        std::conjunction_v<std::is_nothrow_default_constructible<T1>, std::is_nothrow_constructible<T2, Second...>>)
+        : first_{}, second_{std::forward<Second>(second)...}
+    {
+    }
+
+    template <class First, class... Second>
+    constexpr CompPair(OneThen, First&& first, Second&&... second) noexcept(
+        std::conjunction_v<std::is_nothrow_constructible<T1, First>, std::is_nothrow_constructible<T2, Second...>>)
+        : first_{std::forward<First>(first)}, second_{std::forward<Second>(second)...}
+    {
+    }
+
+    [[nodiscard]] constexpr T1& first() noexcept
+    {
+        return first_;
+    }
+
+    [[nodiscard]] constexpr T2& second() noexcept
+    {
+        return second_;
+    }
+
+    [[nodiscard]] constexpr const T1& first() const noexcept
+    {
+        return first_;
+    }
+
+    [[nodiscard]] constexpr const T2& second() const noexcept
+    {
+        return second_;
+    }
+
+private:
+    T1 first_;
+    T2 second_;
+};
 }
--- Source/Runtime/Renderer/Public/Interfaces/Camera.hpp
+++ Source/Runtime/Renderer/Public/Interfaces/Camera.hpp
@@ -2,20 +2,20 @@
 
 namespace oeng::renderer
 {
-	class RENDERER_API ICamera
-	{
-	public:
-		INTERFACE_BODY(ICamera);
+class RENDERER_API ICamera
+{
+public:
+    INTERFACE_BODY(ICamera);
 
-		struct Data
-		{
-			Float near, far;
-			Rad vfov;
-		};
+    struct Data
+    {
+        Float near, far;
+        Rad vfov;
+    };
 
-		[[nodiscard]] virtual const Data& GetData() const noexcept = 0;
-		[[nodiscard]] virtual const Vec3& GetPos() const noexcept = 0;
-		[[nodiscard]] virtual const Mat4& GetViewProj() const noexcept = 0;
-		virtual void OnScreenSizeChanged(Vec2u16 scr) = 0;
-	};
+    [[nodiscard]] virtual const Data& GetData() const noexcept = 0;
+    [[nodiscard]] virtual const Vec3& GetPos() const noexcept = 0;
+    [[nodiscard]] virtual const Mat4& GetViewProj() const noexcept = 0;
+    virtual void OnScreenSizeChanged(Vec2u16 scr) = 0;
+};
 }
