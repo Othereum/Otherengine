e16b355f518f5cf88b2081ba9fff39b1e99dbfe3
Othereum <seokjin.dev@gmail.com>
2020-08-17 16:18:38 +0900

    feat: Implement Load()

--- Source/Runtime/Engine/Public/AssetManager.hpp
+++ Source/Runtime/Engine/Public/AssetManager.hpp
@@ -1,11 +1,20 @@
 #pragma once
-#include "Object.hpp"
+#include "Serialization/ArchiveFileReader.hpp"
+#include "Serialization/Serializable.hpp"
+
+namespace logcat
+{
+extern ENGINE_API const LogCategory kAsset;
+}
 
 namespace oeng::engine
 {
 class ENGINE_API AssetManager
 {
 public:
+    DELETE_CPMV(AssetManager);
+    ~AssetManager();
+
     /**
      * Returns the asset if it is already loaded, otherwise load it.
      * @tparam T Asset type. Must match actual type.
@@ -14,24 +23,31 @@ public:
      * @throw std::bad_cast If the type `T` does not match the type of already loaded asset.
      * @throw std::exception Any exceptions thrown from the constructor of type `T`.
      */
-    template <std::derived_from<OObject> T>
+    template <std::derived_from<ISerializable> T>
     [[nodiscard]] SharedRef<T> Load(Path path)
     {
         if (const auto found = assets_.find(path); found != assets_.end())
-            return DynamicCast<T>(SharedRef{found->second});
-
-        SharedRef loaded{new T{path}, [this, path](T* p)
         {
-            EXPECT(assets_.erase(path) == 1);
-            delete p;
-        }};
-        assets_.emplace(path, loaded);
+            if (const auto ptr = found->second.lock())
+                return DynamicCast<T>(SharedRef{std::move(ptr)});
+
+#ifndef NDEBUG
+            ++reload_count_[path];
+#endif
+        }
+
+        ArchiveFileReader file{path};
+        auto loaded = MakeShared<T>();
+        file << *loaded;
+
+        assets_.insert_or_assign(path, loaded);
         return loaded;
     }
 
-    template <std::constructible_from<>>
-
 private:
-    std::unordered_map<Path, WeakPtr<OObject>> assets_;
+    std::unordered_map<Path, WeakPtr<ISerializable>> assets_;
+#ifndef NDEBUG
+    std::unordered_map<Path, unsigned> reload_count_;
+#endif
 };
 }
