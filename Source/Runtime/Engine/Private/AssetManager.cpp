a6676fbf9bd00ceaca3ec87663f83e6e232e066c
Othereum <seokjin.dev@gmail.com>
2020-08-17 16:19:11 +0900

    feat: Logging asset reload count

---
+++ Source/Runtime/Engine/Private/AssetManager.cpp
@@ -0,0 +1,26 @@
+#include "AssetManager.hpp"
+
+namespace logcat
+{
+const LogCategory kAsset{u8"Asset"sv};
+}
+
+namespace oeng::engine
+{
+AssetManager::~AssetManager()
+#ifndef NDEBUG
+{
+    if (!reload_count_.empty())
+    {
+        OE_LOG(kAsset, kDebug, u8"Asset reload count"sv);
+
+        for (const auto& [path, count] : reload_count_)
+        {
+            OE_LOG(kAsset, kDebug, u8"{}: {} times"sv, path.Str(), count);
+        }
+    }
+}
+#else
+= default;
+#endif
+}
