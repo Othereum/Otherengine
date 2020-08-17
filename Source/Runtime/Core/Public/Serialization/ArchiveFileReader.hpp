3b636b1e2f482a8933772e92c891dc7b71b31b4a
Othereum <seokjin.dev@gmail.com>
2020-08-17 16:18:16 +0900

    feat: ArchiveFileReader constructor

--- Source/Runtime/Core/Public/Serialization/ArchiveFileReader.hpp
+++ Source/Runtime/Core/Public/Serialization/ArchiveFileReader.hpp
@@ -7,6 +7,11 @@ namespace oeng::core
 class ArchiveFileReader final : public Archive
 {
 public:
+    explicit ArchiveFileReader(const fs::path& filepath)
+        : stream_{filepath, std::ios_base::in | std::ios_base::binary}
+    {
+    }
+
     [[nodiscard]] bool IsLoading() const noexcept override
     {
         return true;
