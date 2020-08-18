3b94e49728dab0703af51df70025ac9c794a7b24
Othereum <seokjin.dev@gmail.com>
2020-08-18 17:20:47 +0900

    feat: Completely new serialization code

--- Source/Runtime/Renderer/Public/VertexArray.hpp
+++ Source/Runtime/Renderer/Public/VertexArray.hpp
@@ -1,85 +1,87 @@
 #pragma once
-#include <span>
 
 namespace oeng::renderer
 {
 struct RENDERER_API Vertex
 {
-    struct Data
-    {
-        Vec3 pos;
-        Vec3 norm;
-        Vec2 uv;
-    };
+    Vec3 pos;
+    Vec3 norm;
+    Vec2 uv;
 
-    union
-    {
-        Data data;
-        Float arr[8];
-    };
+    constexpr Vertex() noexcept = default;
 
-    constexpr Vertex() noexcept
-        : data{}
+    constexpr Vertex(const Vec3& pos, const Vec3& norm, const Vec2& uv) noexcept
+        : pos{pos}, norm{norm}, uv{uv}
     {
     }
 
-    constexpr Vertex(const Vec3& pos, const Vec3& norm, const Vec2& uv) noexcept
-        : data{pos, norm, uv}
+    [[nodiscard]] auto& AsArray() noexcept
     {
+        return reinterpret_cast<Float(&)[8]>(*this);
+    }
+
+    [[nodiscard]] auto& AsArray() const noexcept
+    {
+        return reinterpret_cast<const Float(&)[8]>(*this);
     }
 };
 
 RENDERER_API void from_json(const Json& json, Vertex& vertex);
+RENDERER_API void to_json(Json& json, const Vertex& vertex);
+
 
 class RENDERER_API VertexArray
 {
 public:
-    VertexArray(std::span<const Vertex> verts, std::span<const Vec3u16> indices);
-
-    void Load(std::span<const Vertex> verts, std::span<const Vec3u16> indices)
-    {
-        VertexArray{verts, indices}.swap(*this);
-    }
-
+    VertexArray() = default;
     ~VertexArray();
 
-    VertexArray(VertexArray&&) = delete;
-    VertexArray(const VertexArray&) = delete;
-    VertexArray& operator=(VertexArray&&) = delete;
-    VertexArray& operator=(const VertexArray&) = delete;
+    VertexArray(std::vector<Vertex> vertices, std::vector<Vec3u16> indices);
+    void Load(std::vector<Vertex> vertices, std::vector<Vec3u16> indices);
+
+    VertexArray(const VertexArray& r);
+    VertexArray(VertexArray&& r) noexcept;
+    VertexArray& operator=(const VertexArray& r);
+    VertexArray& operator=(VertexArray&& r) noexcept;
 
     void Activate() const;
 
-    [[nodiscard]] size_t GetNumVerts() const noexcept
+    [[nodiscard]] bool IsValid() const noexcept
     {
-        return num_verts_;
+        return vertex_array_ != 0;
     }
 
-    [[nodiscard]] size_t GetNumIndices() const noexcept
+    [[nodiscard]] auto& Vertices() const noexcept
     {
-        return num_indices_;
+        return vertices_;
     }
 
-    void swap(VertexArray& r) noexcept
+    [[nodiscard]] auto& Indices() const noexcept
     {
-        using std::swap;
-        swap(num_verts_, r.num_verts_);
-        swap(num_indices_, r.num_indices_);
-        swap(vertex_buffer_, r.vertex_buffer_);
-        swap(index_buffer_, r.index_buffer_);
-        swap(vertex_array_, r.vertex_array_);
+        return indices_;
     }
+
+    void swap(VertexArray& r) noexcept;
+
+    friend RENDERER_API Archive& operator<<(Archive& archive, VertexArray& vertex_array);
 
 private:
-    size_t num_verts_;
-    size_t num_indices_;
-    unsigned vertex_buffer_{};
-    unsigned index_buffer_{};
-    unsigned vertex_array_{};
+    void Generate();
+    void CalcRadius();
+
+    std::vector<Vertex> vertices_;
+    std::vector<Vec3u16> indices_;
+    Float radius_ = 0;
+    unsigned vertex_array_ = 0;
+    unsigned vertex_buffer_ = 0;
+    unsigned index_buffer_ = 0;
 };
 
 inline void swap(VertexArray& a, VertexArray& b) noexcept
 {
     a.swap(b);
 }
+
+RENDERER_API void from_json(const Json& json, VertexArray& vertex_array);
+RENDERER_API void to_json(Json& json, const VertexArray& vertex_array);
 }
