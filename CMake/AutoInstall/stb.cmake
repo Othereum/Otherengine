40db92fa5519c317fc8e14c7f83d18622567103c
Othereum <seokjin.dev@gmail.com>
2020-08-20 01:37:23 +0900

    chore: Add stb

--- CMake/AutoInstall/stb.cmake
+++ CMake/AutoInstall/stb.cmake
@@ -3,8 +3,7 @@ project(@name@-download NONE)
 
 include(ExternalProject)
 ExternalProject_Add(@name@-download
-	GIT_REPOSITORY		https://github.com/SpartanJ/SOIL2
-	GIT_TAG				release-1.20
+	GIT_REPOSITORY		https://github.com/nothings/stb
 	GIT_SHALLOW			TRUE
 	GIT_PROGRESS		TRUE
 	SOURCE_DIR			"@src_dir@"
@@ -14,5 +13,3 @@ ExternalProject_Add(@name@-download
 	INSTALL_COMMAND		""
 	TEST_COMMAND		""
 )
-
-configure_file("@cmake_dir@/soil2-build.cmake" "@src_dir@/CMakeLists.txt" @ONLY)
