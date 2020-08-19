02e8c0d3412896fe21f55eb73db328bc546db279
Othereum <seokjin.dev@gmail.com>
2020-08-19 15:20:30 +0900

    feat: ReadJsonFile(), WriteJsonFile()

--- Source/Runtime/Core/Public/Json.hpp
+++ Source/Runtime/Core/Public/Json.hpp
@@ -3,14 +3,42 @@
 #undef JSON_USE_IMPLICIT_CONVERSIONS
 #define JSON_USE_IMPLICIT_CONVERSIONS 0
 
+#include "File.hpp"
 #include <nlohmann/json.hpp>
 
 namespace oeng
 {
 namespace core
 {
+namespace fs = std::filesystem;
 using Json = nlohmann::json;
 using JsonType = nlohmann::detail::value_t;
+using JsonParseError = nlohmann::detail::parse_error;
+
+/**
+ * Read json file.
+ * @param filepath File path.
+ * @return Json object.
+ * @throw std::ifstream::failure If failed to open file.
+ * @throw JsonParseError If failed to parse.
+ */
+[[nodiscard]] inline Json ReadJsonFile(const fs::path& filepath)
+{
+    return Json::parse(ReadFile(filepath), nullptr, true, true);
+}
+
+/**
+ * Write json file.
+ * If there are no directories corresponding to the specified path, they are automatically created.
+ * @param filepath File path.
+ * @param json Json object.
+ * @throw std::ofstream::failure If failed to open file.
+ * @throw std::filesystem::filesystem_error If the path is invalid.
+ */
+inline void WriteJsonFile(const fs::path& filepath, const Json& json)
+{
+    WriteFile(filepath) << json.dump(4);
+}
 }
 
 using namespace core;
