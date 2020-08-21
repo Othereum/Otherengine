13889384e4f46cae95c46a33ef341f2569ef79c2
Othereum <seokjin.dev@gmail.com>
2020-08-21 20:21:44 +0900

    feat: engine::Mesh

---
+++ Source/Runtime/Engine/Private/Engine/Mesh.cpp
@@ -0,0 +1,26 @@
+#include "Engine/Mesh.hpp"
+#include "AssetManager.hpp"
+#include "DynamicRHI.hpp"
+#include "Vertex.hpp"
+
+namespace oeng
+{
+inline namespace engine
+{
+void Mesh::Serialize(Archive& ar)
+{
+    const auto json = ar.ReadAllAsJson();
+
+    const auto vertices = json.at("vertices").get<std::vector<Vertex>>();
+    const auto indices = json.at("indices").get<std::vector<Vec3u16>>();
+
+    material_ = AssetManager::Get().Load<Material>(json.at("material").get<Path>());
+    rhi_.reset(DynamicRHI::Get().CreateMesh(vertices, indices));
+
+    auto max = 0_f;
+    for (const auto& v : vertices)
+        max = Max(max, v.pos.DistSqr(Vec3::zero));
+    radius_ = std::sqrt(max);
+}
+}
+}
---
+++ Source/Runtime/Engine/Public/Engine/Mesh.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace oeng
+{
+inline namespace rhi
+{
+class RHIResource;
+}
+}
+
+namespace oeng
+{
+inline namespace engine
+{
+class ENGINE_API Mesh : public ISerializable
+{
+public:
+    void Serialize(Archive& ar) override;
+
+    [[nodiscard]] Float Radius() const noexcept
+    {
+        return radius_;
+    }
+
+private:
+    SharedPtr<class Material> material_;
+    std::unique_ptr<RHIResource> rhi_;
+    Float radius_ = 0;
+};
+}
+}
