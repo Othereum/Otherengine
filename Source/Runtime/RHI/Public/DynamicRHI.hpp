90a1ee118adab16a6e1388251c57182b3fae51d4
Othereum <seokjin.dev@gmail.com>
2020-08-21 20:20:11 +0900

    feat: CreateMesh()

--- Source/Runtime/RHI/Public/DynamicRHI.hpp
+++ Source/Runtime/RHI/Public/DynamicRHI.hpp
@@ -1,16 +1,21 @@
 #pragma once
+#include <span>
 
 namespace oeng
 {
 inline namespace rhi
 {
+class RHIResource;
+
 class RHI_API DynamicRHI
 {
 public:
     INTERFACE_BODY(DynamicRHI);
 
     [[nodiscard]] static DynamicRHI& Get() noexcept;
-    [[nodiscard]] virtual class RHIResource* CreateTexture(Vec2i size, int channels, const void* pixels) const = 0;
+    [[nodiscard]] virtual RHIResource* CreateTexture(Vec2i size, int channels, const void* pixels) const = 0;
+    [[nodiscard]] virtual RHIResource* CreateMesh(std::span<const struct Vertex> vertices,
+                                                  std::span<const Vec3u16> indices) const = 0;
 };
 }
 }
