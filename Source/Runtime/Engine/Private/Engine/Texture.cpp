8196d52e878e7fc77c0931851336970b48598051
Othereum <seokjin.dev@gmail.com>
2020-08-21 20:21:32 +0900

    refactor: Use ReadAll()

--- Source/Runtime/Engine/Private/Engine/Texture.cpp
+++ Source/Runtime/Engine/Private/Engine/Texture.cpp
@@ -8,15 +8,12 @@ inline namespace engine
 {
 void Texture::Serialize(Archive& ar)
 {
-    assert(ar.IsLoading());
-
-    const auto len = ar.Size();
-    const std::unique_ptr<unsigned char[]> raw{new unsigned char[len]};
-    ar.Serialize(raw.get(), len);
+    const auto [raw, len] = ar.ReadAll<unsigned char>();
 
     int channels;
     const std::unique_ptr<unsigned char[]> pixels{
         stbi_load_from_memory(raw.get(), len, &size_[0], &size_[1], &channels, 0)};
+
     rhi_.reset(DynamicRHI::Get().CreateTexture(size_, channels, pixels.get()));
 }
 }
