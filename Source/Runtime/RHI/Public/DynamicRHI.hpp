e73aa79d89ef921f9d4fcb5ba1bff452a17cfd01
Othereum <seokjin.dev@gmail.com>
2020-08-22 17:52:11 +0900

    feat: DynamicRHI::CreateShader()

--- Source/Runtime/RHI/Public/DynamicRHI.hpp
+++ Source/Runtime/RHI/Public/DynamicRHI.hpp
@@ -6,16 +6,20 @@ namespace oeng
 inline namespace rhi
 {
 class RHIResource;
+class Shader;
+struct Vertex;
 
 class RHI_API DynamicRHI
 {
 public:
     INTERFACE_BODY(DynamicRHI);
-
     [[nodiscard]] static DynamicRHI& Get() noexcept;
+
     [[nodiscard]] virtual RHIResource* CreateTexture(Vec2i size, int channels, const void* pixels) const = 0;
-    [[nodiscard]] virtual RHIResource* CreateMesh(std::span<const struct Vertex> vertices,
+    [[nodiscard]] virtual RHIResource* CreateMesh(std::span<const Vertex> vertices,
                                                   std::span<const Vec3u16> indices) const = 0;
+
+    [[nodiscard]] virtual Shader* CreateShader(const char* vertex_shader, const char* frag_shader) const = 0;
 };
 }
 }
