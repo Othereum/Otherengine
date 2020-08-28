239988b154d9a966f41f72f3c67e64635ff65a6c
Othereum <seokjin.dev@gmail.com>
2020-08-28 23:58:37 +0900

    feat: Implement LoadParams()

--- Source/Runtime/Engine/Private/Materials/MaterialInterface.cpp
+++ Source/Runtime/Engine/Private/Materials/MaterialInterface.cpp
@@ -1,15 +1,35 @@
 #include "Materials/MaterialInterface.hpp"
+#include "AssetManager.hpp"
 
 namespace oeng
 {
 inline namespace engine
 {
+template <class T, class Fn>
+void LoadParam(const Json& json, const std::string& name, std::unordered_map<Name, T>& out, Fn&& fn)
+{
+    out.clear();
+    for (const auto& [name, value] : json.at(name).items())
+        out.emplace(AsString8(name), fn(value));
+}
+
+template <class T>
+void LoadParam(const Json& json, const std::string& name, std::unordered_map<Name, T>& out)
+{
+    LoadParam(json, name, out, [](const Json& value)
+    {
+        return value.get<T>();
+    });
+}
+
 void MaterialInterface::LoadParams(const Json& json)
 {
-    for (const auto& [name, value] : json.at("scalars").items())
+    LoadParam(json, "scalars"s, scalars_);
+    LoadParam(json, "vectors"s, vectors_);
+    LoadParam(json, "textures"s, textures_, [](const Json& value)
     {
-        scalars_.emplace(AsString8(name), value.get<Float>());
-    }
+        return AssetManager::Get().Load<Texture>(value.get<Path>());
+    });
 }
 }
 }
