b78aaea08e733e66a7cc248b4690893be2b72e51
Othereum <seokjin.dev@gmail.com>
2020-08-31 22:46:12 +0900

    feat: Determine loading asset type at runtime

--- Source/Runtime/Engine/Public/AssetManager.hpp
+++ Source/Runtime/Engine/Public/AssetManager.hpp
@@ -1,6 +1,6 @@
 #pragma once
+#include "Object/Object.hpp"
 #include "Serialization/ArchiveFileReader.hpp"
-#include "Serialization/Serializable.hpp"
 
 namespace logcat
 {
@@ -22,16 +22,16 @@ public:
      * @tparam T Asset type. Must match actual type.
      * @param path Asset path.
      * @return Loaded asset.
-     * @throw std::bad_cast If the type `T` does not match the type of already loaded asset.
+     * @throw std::bad_cast If failed to cast from actual type of asset to type `T`.
      * @throw std::exception Any exceptions thrown from the constructor of type `T`.
      */
-    template <std::derived_from<ISerializable> T>
-    [[nodiscard]] std::shared_ptr<T> Load(Path path)
+    template <class T>
+    [[nodiscard]] std::enable_if_t<std::convertible_to<T*, Object*>, std::shared_ptr<T>> Load(Path path)
     {
         if (const auto found = assets_.find(path); found != assets_.end())
         {
             if (const auto ptr = found->second.lock())
-                return DynamicCast<T>(std::shared_ptr{std::move(ptr)});
+                return {std::move(ptr), &dynamic_cast<T&>(*ptr)};
 
 #ifndef NDEBUG
             ++reload_count_[path];
@@ -39,7 +39,10 @@ public:
         }
 
         ArchiveFileReader file{path};
-        auto loaded = std::make_shared<T>();
+        Name class_name;
+        file << class_name;
+
+        auto loaded = Factory::Get().Create<T>(class_name);
         file << *loaded;
 
         assets_.insert_or_assign(path, loaded);
@@ -47,7 +50,7 @@ public:
     }
 
 private:
-    std::unordered_map<Path, std::weak_ptr<ISerializable>> assets_;
+    std::unordered_map<Path, std::weak_ptr<Object>> assets_;
 #ifndef NDEBUG
     std::unordered_map<Path, unsigned> reload_count_;
 #endif
