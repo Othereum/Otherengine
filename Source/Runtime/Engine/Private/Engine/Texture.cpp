5ed9b8c586687f764bba2146c82a6737d3379dfd
Othereum <seokjin.dev@gmail.com>
2020-09-01 17:20:20 +0900

    feat: Texture::from_json

--- Source/Runtime/Engine/Private/Engine/Texture.cpp
+++ Source/Runtime/Engine/Private/Engine/Texture.cpp
@@ -6,13 +6,13 @@ namespace oeng
 {
 inline namespace engine
 {
-void Texture::Serialize(Archive& ar)
+void Texture::from_json(const Json& json)
 {
-    const auto [raw, len] = ar.ReadAll<unsigned char>();
-
+    const auto src = json.at("texture").get<std::string>();
+    
     int channels;
     const std::unique_ptr<unsigned char[]> pixels{
-        stbi_load_from_memory(raw.get(), len, &size_[0], &size_[1], &channels, 0)};
+        stbi_load(src.c_str(), &size_[0], &size_[1], &channels, 0)};
 
     rhi_.reset(DynamicRHI::Get().CreateTexture(size_, channels, pixels.get()));
 }
--- Source/Runtime/Engine/Public/Engine/Texture.hpp
+++ Source/Runtime/Engine/Public/Engine/Texture.hpp
@@ -9,10 +9,10 @@ class RHITexture;
 
 inline namespace engine
 {
-class ENGINE_API Texture : public ISerializable
+class ENGINE_API Texture final : public Object
 {
 public:
-    void Serialize(Archive& ar) override;
+    void from_json(const Json& json) override;
 
     [[nodiscard]] Vec2i Size() const noexcept
     {
