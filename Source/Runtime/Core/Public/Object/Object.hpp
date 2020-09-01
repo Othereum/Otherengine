35405b47bff54c5a1723482bc9906a1f3cec6dff
Othereum <seokjin.dev@gmail.com>
2020-09-01 17:18:30 +0900

    feat: Object::GetPath()

--- Source/Runtime/Core/Public/Object/Object.hpp
+++ Source/Runtime/Core/Public/Object/Object.hpp
@@ -11,9 +11,24 @@ INTERFACE_BODY(Object)
 public:
     [[nodiscard]] virtual Name GetClassName() const noexcept = 0;
 
+    void from_json(const Json& json, Path path)
+    {
+        path_ = path;
+        from_json(json);
+    }
+
+    [[nodiscard]] Path GetPath() const noexcept
+    {
+        return path_;
+    }
+
+protected:
     virtual void from_json([[maybe_unused]] const Json& json)
     {
     }
+
+private:
+    Path path_;
 };
 }
 }
