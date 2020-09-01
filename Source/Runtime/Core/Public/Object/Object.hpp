3b0feb398993ad5aa16e462de081fcbefb824ec7
Othereum <seokjin.dev@gmail.com>
2020-09-01 15:17:58 +0900

    feat: Replace Serialize() to from_json()
    because serialization feature is postponed

--- Source/Runtime/Core/Public/Object/Object.hpp
+++ Source/Runtime/Core/Public/Object/Object.hpp
@@ -9,14 +9,11 @@ class CORE_API Object : public std::enable_shared_from_this<Object>
 INTERFACE_BODY(Object)
 
 public:
-    virtual void Serialize(class Archive& ar) = 0;
     [[nodiscard]] virtual Name GetClassName() const noexcept = 0;
-};
 
-inline Archive& operator<<(Archive& ar, Object& obj)
-{
-    obj.Serialize(ar);
-    return ar;
-}
+    virtual void from_json([[maybe_unused]] const Json& json)
+    {
+    }
+};
 }
 }
