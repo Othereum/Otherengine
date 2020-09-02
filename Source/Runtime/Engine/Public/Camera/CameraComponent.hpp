bbbabe4a89db35c138e9e3cbdfffb053bd296027
Othereum <seokjin.dev@gmail.com>
2020-09-02 20:52:18 +0900

    refactor: CameraComponent

--- Source/Runtime/Engine/Public/Camera/CameraComponent.hpp
+++ Source/Runtime/Engine/Public/Camera/CameraComponent.hpp
@@ -1,37 +1,53 @@
 #pragma once
 #include "Components/SceneComponent.hpp"
-#include "Interfaces/Camera.hpp"
 
-namespace oeng::engine
+namespace oeng
 {
-	class ENGINE_API CameraComponent : public SceneComponent, public ICamera
-	{
-	public:
-		DELETE_CPMV(CameraComponent);
-		
-		explicit CameraComponent(AActor& owner, int update_order = 100);
-		~CameraComponent();
+inline namespace engine
+{
+class ENGINE_API CameraComponent final : public SceneComponent
+{
+CLASS_BODY(CameraComponent)
 
-		void SetVFov(Rad vfov) noexcept { data_.vfov = vfov; RecalcProj(); }
-		void SetNearFar(Float near, Float far) noexcept { data_.near = near; data_.far = far; RecalcProj(); }
+public:
+    struct Data
+    {
+        Rad vfov = 60_deg;
+        Float near = 10, far = 10000;
+    };
 
-		const Vec3& GetPos() const noexcept override;
-		const Data& GetData() const noexcept override { return data_;}
-		const Mat4& GetViewProj() const noexcept override;
-		void OnScreenSizeChanged(Vec2u16 scr) noexcept override;
+    explicit CameraComponent(AActor& owner, int update_order = 100);
+    ~CameraComponent();
 
-	protected:
-		void OnTrsfChanged() noexcept override;
-		void OnActivated() override;
-		void OnDeactivated() override;
+    void SetData(const Data& data) noexcept
+    {
+        data_ = data;
+        RecalcProj();
+    }
 
-	private:
-		void RecalcView() noexcept;
-		void RecalcProj() noexcept;
-		
-		Mat4 view_, proj_;
-		Mat4 view_proj_;
-		Data data_{10, 10000, 60_deg};
-		Vec2 scr_;
-	};
+    [[nodiscard]] const Data& GetData() const noexcept
+    {
+        return data_;
+    }
+
+    [[nodiscard]] const Vec3& GetPos() const noexcept;
+    [[nodiscard]] const Mat4& GetViewProj() const noexcept;
+
+    void OnScreenSizeChanged(Vec2u16 scr) noexcept;
+
+protected:
+    void OnTrsfChanged() noexcept override;
+    void OnActivated() override;
+    void OnDeactivated() override;
+
+private:
+    void RecalcView() noexcept;
+    void RecalcProj() noexcept;
+
+    Mat4 view_, proj_;
+    Mat4 view_proj_;
+    Data data_;
+    Vec2 scr_;
+};
+}
 }
