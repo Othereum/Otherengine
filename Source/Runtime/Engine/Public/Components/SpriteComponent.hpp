10a7d4d208d7c6b0a0c9f59cd3ce44e9649cd2a3
Othereum <seokjin.dev@gmail.com>
2020-09-02 20:53:40 +0900

    refactor: SpriteComponent

--- Source/Runtime/Engine/Public/Components/SpriteComponent.hpp
+++ Source/Runtime/Engine/Public/Components/SpriteComponent.hpp
@@ -1,39 +1,53 @@
 #pragma once
-#include "Interfaces/Drawable.hpp"
 #include "SceneComponent.hpp"
-#include "Path.hpp"
 
 struct SDL_Renderer;
 
-namespace oeng::renderer
+namespace oeng
 {
-	class Renderer;
-	class Texture;
+inline namespace engine
+{
+class Texture;
+
+class ENGINE_API SpriteComponent final : public SceneComponent
+{
+CLASS_BODY(SpriteComponent)
+
+public:
+    explicit SpriteComponent(AActor& owner, int draw_order = 100, int update_order = 100);
+    ~SpriteComponent();
+
+    [[nodiscard]] bool ShouldDraw() const noexcept
+    {
+        return IsActive();
+    }
+
+    [[nodiscard]] int GetDrawOrder() const noexcept
+    {
+        return draw_order_;
+    }
+
+    [[nodiscard]] const Mat4& GetDrawTrsf() const noexcept
+    {
+        return GetWorldTrsfMatrix();
+    }
+
+    void SetTexture(std::shared_ptr<Texture> texture) noexcept
+    {
+        texture_ = std::move(texture);
+    }
+
+    [[nodiscard]] Texture& GetTexture() const noexcept
+    {
+        return *texture_;
+    }
+
+protected:
+    void OnBeginPlay() override;
+
+private:
+    std::shared_ptr<Texture> texture_;
+    int draw_order_;
+};
 }
-
-namespace oeng::engine
-{
-	class ENGINE_API SpriteComponent : public SceneComponent, public ISpriteComponent
-	{
-	public:
-		DELETE_CPMV(SpriteComponent);
-		
-		explicit SpriteComponent(AActor& owner, int draw_order = 100, int update_order = 100);
-		~SpriteComponent();
-
-		[[nodiscard]] bool ShouldDraw() const noexcept override { return IsActive(); }
-		[[nodiscard]] int GetDrawOrder() const noexcept override { return draw_order_; }
-		[[nodiscard]] const Mat4& GetDrawTrsf() const noexcept override { return GetWorldTrsfMatrix(); }
-
-		void SetTexture(Path file);
-		void SetTexture(std::shared_ptr<Texture> texture) noexcept { texture_ = std::move(texture); }
-		[[nodiscard]] Texture& GetTexture() const noexcept override { return *texture_; }
-
-	protected:
-		void OnBeginPlay() override;
-
-	private:
-		std::shared_ptr<Texture> texture_;
-		int draw_order_;
-	};
 }
