c6646cf711e0aa51dc9d70479f3642e0d698065b
Othereum <seokjin.dev@gmail.com>
2020-09-03 16:21:48 +0900

    feat: Seperated updated actor list

--- Source/Runtime/Engine/Private/World.cpp
+++ Source/Runtime/Engine/Private/World.cpp
@@ -4,79 +4,89 @@
 #include "Actors/Actor.hpp"
 #include "Components/SphereComponent.hpp"
 
-namespace oeng::engine
+namespace oeng
 {
-	World::World()
-		:timer_{*this}, delta_seconds_{0}
-	{
-	}
-
-	World::~World() = default;
-
-	void World::BeginTick()
-	{
-		time_ = Clock::now();
-	}
-
-	void World::Tick()
-	{
-		SCOPE_STACK_COUNTER(WorldTick);
-		UpdateTime();
-		UpdateGame();
-	}
-
-	void World::RegisterCollision(SphereComponent& comp)
-	{
-		collisions_.emplace_back(comp);
-	}
-
-	void World::UnregisterCollision(SphereComponent& comp)
-	{
-		auto pr = [&](const SphereComponent& v) { return &v == &comp; };
-		const auto found = std::find_if(collisions_.crbegin(), collisions_.crend(), pr);
-		if (found != collisions_.crend()) collisions_.erase(found.base() - 1);
-	}
-
-	void World::UpdateGame()
-	{
-		for (const auto& actor : actors_)
-			actor->Update(delta_seconds_);
-		
-		for (size_t i = 0; i < collisions_.size(); ++i)
-			for (auto j = i+1; j < collisions_.size(); ++j)
-				collisions_[i].get().DoOverlap(collisions_[j]);
-
-		timer_.Update();
-
-		// Use index-based loop because BeginPlay() can spawn new actors
-		for (size_t i=0; i<pending_actors_.size(); ++i)
-		{
-			auto& actor = *pending_actors_[i];
-			actors_.push_back(std::move(pending_actors_[i]));
-			actor.BeginPlay();
-		}
-		pending_actors_.clear();
-
-		for (auto it = actors_.rbegin(); it != actors_.rend();)
-		{
-			const auto& actor = **it;
-			if (actor.IsPendingKill())
-			{
-				const auto next = actors_.erase(it.base() - 1);
-				it = make_reverse_iterator(next);
-			}
-			else
-			{
-				++it;
-			}
-		}
-	}
-
-	void World::UpdateTime()
+inline namespace engine
 {
-		const auto now = Clock::now();
-		delta_seconds_ = time::duration<Float>{now - time_}.count();
-		delta_seconds_ = Min(1_f, delta_seconds_);
-		time_ = now;
-	}
+World::World()
+    : timer_{*this}, delta_seconds_{0}
+{
+}
+
+World::~World() = default;
+
+void World::BeginTick()
+{
+    time_ = Clock::now();
+}
+
+void World::Tick()
+{
+    SCOPE_STACK_COUNTER(WorldTick);
+    UpdateTime();
+    UpdateGame();
+}
+
+void World::AddCollision(SphereComponent& comp)
+{
+    collisions_.emplace_back(comp);
+}
+
+void World::RemoveCollision(SphereComponent& comp)
+{
+    auto pr = [&](const SphereComponent& v)
+    {
+        return &v == &comp;
+    };
+    const auto found = std::find_if(collisions_.crbegin(), collisions_.crend(), pr);
+    if (found != collisions_.crend())
+        collisions_.erase(found.base() - 1);
+}
+
+void World::UpdateGame()
+{
+    for (auto* actor : updated_actors_)
+        actor->Update(delta_seconds_);
+
+    for (size_t i = 0; i < collisions_.size(); ++i)
+        for (auto j = i + 1; j < collisions_.size(); ++j)
+            collisions_[i]->DoOverlap(*collisions_[j]);
+
+    timer_.Update();
+
+    // Use index-based loop because BeginPlay() can spawn new actors
+    for (size_t i = 0; i < actors_.size(); ++i)
+    {
+        if (actors_[i]->can_ever_update_)
+            updated_actors_.push_back(actors_[i].get());
+
+        actors_[i]->BeginPlay();
+    }
+
+    for (auto it = actors_.rbegin(); it != actors_.rend();)
+    {
+        const auto& actor = **it;
+        if (actor.IsPendingKill())
+        {
+            if (actor.can_ever_update_)
+                updated_actors_.erase(std::find(updated_actors_.begin(), updated_actors_.end(), &actor));
+
+            const auto next = actors_.erase(it.base() - 1);
+            it = make_reverse_iterator(next);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+}
+
+void World::UpdateTime()
+{
+    const auto now = Clock::now();
+    delta_seconds_ = time::duration<Float>{now - time_}.count();
+    delta_seconds_ = Min(1_f, delta_seconds_);
+    time_ = now;
+}
+}
 }
--- Source/Runtime/Engine/Public/World.hpp
+++ Source/Runtime/Engine/Public/World.hpp
@@ -1,59 +1,74 @@
 #pragma once
 #include "TimerManager.hpp"
 
-namespace oeng::engine
+namespace oeng
 {
-	class AActor;
-	class SphereComponent;
-	
-	class ENGINE_API World
-	{
-	public:
-		DELETE_CPMV(World);
-		
-		World();
-		~World();
+inline namespace engine
+{
+class AActor;
+class SphereComponent;
 
-		void BeginTick();
-		void Tick();
-		
-		template <std::derived_from<AActor> T, class... Args>
-		std::shared_ptr<T> SpawnActor(Args&&... args)
-		{
-			static_assert(std::is_constructible_v<T, World&, Args...>);
-			auto ptr = std::make_shared<T>(*this, std::forward<Args>(args)...);
-			pending_actors_.emplace_back(ptr);
-			return ptr;
-		}
+class ENGINE_API World
+{
+public:
+    DELETE_CPMV(World);
 
-		template <std::invocable<AActor&> Fn>
-		void ForEachActor(Fn&& fn) const
-		{
-			for (auto& actor : actors_)
-			{
-				fn(*actor);
-			}
-		}
+    World();
+    ~World();
 
-		void RegisterCollision(SphereComponent& comp);
-		void UnregisterCollision(SphereComponent& comp);
-		
-		[[nodiscard]] TimerManager& GetTimerManager() noexcept { return timer_; }
-		[[nodiscard]] TimePoint GetTime() const noexcept { return time_; }
-		[[nodiscard]] Float GetDeltaSeconds() const noexcept { return delta_seconds_; }
+    void BeginTick();
+    void Tick();
 
-	private:
-		void UpdateGame();
-		void UpdateTime();
+    template <class T>
+    T& SpawnActor()
+    {
+        auto ptr = std::make_shared<T>();
+        auto& ref = *ptr;
+        ref.world_ = this;
+        actors_.emplace_back(std::move(ptr));
+        return ref;
+    }
 
-		TimerManager timer_;
+    template <std::invocable<AActor&> Fn>
+    void ForEachActor(Fn&& fn) const
+    {
+        for (auto& actor : actors_)
+        {
+            fn(*actor);
+        }
+    }
 
-		DyArr<std::reference_wrapper<SphereComponent>> collisions_;
-		
-		DyArr<std::shared_ptr<AActor>> actors_;
-		DyArr<std::shared_ptr<AActor>> pending_actors_;
-		
-		TimePoint time_;
-		Float delta_seconds_;
-	};
+    void AddCollision(SphereComponent& comp);
+    void RemoveCollision(SphereComponent& comp);
+
+    [[nodiscard]] TimerManager& GetTimerManager() noexcept
+    {
+        return timer_;
+    }
+
+    [[nodiscard]] TimePoint GetTime() const noexcept
+    {
+        return time_;
+    }
+
+    [[nodiscard]] Float GetDeltaSeconds() const noexcept
+    {
+        return delta_seconds_;
+    }
+
+private:
+    void UpdateGame();
+    void UpdateTime();
+
+    TimerManager timer_;
+
+    std::vector<SphereComponent*> collisions_;
+
+    std::vector<std::shared_ptr<AActor>> actors_;
+    std::vector<AActor*> updated_actors_;
+
+    TimePoint time_;
+    Float delta_seconds_;
+};
+}
 }
