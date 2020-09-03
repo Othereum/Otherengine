9b9ada96785a0b4d54c3a63c201fd9e9ee54c529
Othereum <seokjin.dev@gmail.com>
2020-09-03 20:30:31 +0900

    feat: New game framework classes

---
+++ Source/Runtime/Engine/Public/Engine/LocalPlayer.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "Player.hpp"
+
+namespace oeng
+{
+inline namespace engine
+{
+class LocalPlayer : public Player
+{
+};
+}
+}
---
+++ Source/Runtime/Engine/Public/GameFramework/GameModeBase.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "Actor.hpp"
+
+namespace oeng
+{
+inline namespace engine
+{
+class AGameModeBase : public AActor
+{
+    
+};
+}
+}
