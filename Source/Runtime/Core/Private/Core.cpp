b0b55d578c92bf2f48f703b166c0451474274766
Othereum <seokjin.dev@gmail.com>
2020-09-04 23:59:17 +0900

    feat: InitLogger();

--- Source/Runtime/Core/Private/Core.cpp
+++ Source/Runtime/Core/Private/Core.cpp
@@ -8,10 +8,13 @@ inline namespace core
 static std::u8string_view game_name;
 static const std::thread::id kGameThreadId = std::this_thread::get_id();
 
+void InitLogger();
+
 void SetGameName(std::u8string_view game_name)
 {
     assert(!game_name.empty());
     core::game_name = game_name;
+    InitLogger();
 }
 
 std::u8string_view GetGameName() noexcept
