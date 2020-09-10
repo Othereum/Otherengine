14cff34e59a9fb89410be3863f334c4fbe135be7
Othereum <seokjin.dev@gmail.com>
2020-09-10 20:23:33 +0900

    feat: UVec3 norm

--- Source/Runtime/RHI/Public/Vertex.hpp
+++ Source/Runtime/RHI/Public/Vertex.hpp
@@ -7,19 +7,13 @@ inline namespace rhi
 struct Vertex
 {
     Vec3 pos;
-    Vec3 norm;
+    UVec3 norm = UVec3::Up();
     Vec2 uv;
 
     constexpr Vertex() noexcept = default;
 
-    constexpr Vertex(const Vec3& pos, const Vec3& norm, const Vec2& uv) noexcept
-        : pos{pos}, norm{norm}, uv{uv}
+    constexpr Vertex(const Vec3& pos, const UVec3& norm, const Vec2& uv) noexcept : pos{pos}, norm{norm}, uv{uv}
     {
-    }
-
-    [[nodiscard]] auto& AsArray() noexcept
-    {
-        return reinterpret_cast<Float(&)[8]>(*this);
     }
 
     [[nodiscard]] auto& AsArray() const noexcept
@@ -30,13 +24,20 @@ struct Vertex
 
 inline void from_json(const Json& json, Vertex& vertex)
 {
-    json.get_to(vertex.AsArray());
-    vertex.norm.Normalize();
+    vertex.pos[0] = json[0].get<Float>();
+    vertex.pos[1] = json[1].get<Float>();
+    vertex.pos[2] = json[2].get<Float>();
+
+    Vec3 norm{json[3].get<Float>(), json[4].get<Float>(), json[5].get<Float>()};
+    vertex.norm = norm.Unit().value();
+
+    vertex.uv[0] = json[6].get<Float>();
+    vertex.uv[1] = json[7].get<Float>();
 }
 
 inline void to_json(Json& json, const Vertex& vertex)
 {
     json = vertex.AsArray();
 }
-}
-}
+} // namespace rhi
+} // namespace oeng
