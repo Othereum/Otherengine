999b7268cc5b55f0804f29c5c1760d7b45e0b92c
Othereum <seokjin.dev@gmail.com>
2020-09-14 15:23:25 +0900

    reformat

--- Source/Runtime/OpenGLDrv/Public/OpenGLDynamicRHI.hpp
+++ Source/Runtime/OpenGLDrv/Public/OpenGLDynamicRHI.hpp
@@ -7,12 +7,12 @@ inline namespace opengldrv
 {
 class OPENGLDRV_API OpenGLDynamicRHI final : public DynamicRHI
 {
-public:
+  public:
     [[nodiscard]] RHITexture* CreateTexture(Vec2i size, int channels, const unsigned char* pixels) const override;
     [[nodiscard]] RHIMesh* CreateMesh(std::span<const Vertex> vertices,
                                       std::span<const Vec3u16> indices) const override;
 
     [[nodiscard]] RHIShader* CreateShader(const char* vertex_shader, const char* frag_shader) const override;
 };
-}
-}
+} // namespace opengldrv
+} // namespace oeng
