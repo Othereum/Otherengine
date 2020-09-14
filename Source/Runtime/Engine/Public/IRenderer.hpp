3670b0f6300de64f8019dca541a7e7096cfce11b
Othereum <seokjin.dev@gmail.com>
2020-09-14 15:17:54 +0900

    feat: Add 'const'

--- Source/Runtime/Engine/Public/IRenderer.hpp
+++ Source/Runtime/Engine/Public/IRenderer.hpp
@@ -18,19 +18,19 @@ class IRenderer
     virtual ~IRenderer() = default;
     virtual void DrawScene(const ViewInfo& view) = 0;
 
-    virtual void AddMesh(MeshComponent& mesh) = 0;
-    virtual void AddSprite(SpriteComponent& sprite) = 0;
-    virtual void AddDirLight(DirLightComponent& light) = 0;
-    virtual void AddSkyLight(SkyLightComponent& light) = 0;
-    virtual void AddSpotLight(SpotLightComponent& light) = 0;
-    virtual void AddPointLight(PointLightComponent& light) = 0;
+    virtual void AddMesh(const MeshComponent& mesh) = 0;
+    virtual void AddSprite(const SpriteComponent& sprite) = 0;
+    virtual void AddDirLight(const DirLightComponent& light) = 0;
+    virtual void AddSkyLight(const SkyLightComponent& light) = 0;
+    virtual void AddSpotLight(const SpotLightComponent& light) = 0;
+    virtual void AddPointLight(const PointLightComponent& light) = 0;
 
-    virtual void RemoveMesh(MeshComponent& mesh) = 0;
-    virtual void RemoveSprite(SpriteComponent& sprite) = 0;
-    virtual void RemoveDirLight(DirLightComponent& light) = 0;
-    virtual void RemoveSkyLight(SkyLightComponent& light) = 0;
-    virtual void RemoveSpotLight(SpotLightComponent& light) = 0;
-    virtual void RemovePointLight(PointLightComponent& light) = 0;
+    virtual void RemoveMesh(const MeshComponent& mesh) = 0;
+    virtual void RemoveSprite(const SpriteComponent& sprite) = 0;
+    virtual void RemoveDirLight(const DirLightComponent& light) = 0;
+    virtual void RemoveSkyLight(const SkyLightComponent& light) = 0;
+    virtual void RemoveSpotLight(const SpotLightComponent& light) = 0;
+    virtual void RemovePointLight(const PointLightComponent& light) = 0;
 };
 
 } // namespace engine
