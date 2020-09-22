5aaf99f8e74cf8cd8d145c36ee83b6365afa4a14
Othereum <seokjin.dev@gmail.com>
2020-09-22 11:39:38 +0900

    refactor: Remove unused code

--- Source/Runtime/RHI/Private/RHIWindow.cpp
+++ Source/Runtime/RHI/Private/RHIWindow.cpp
@@ -5,36 +5,6 @@ namespace oeng
 {
 inline namespace rhi
 {
-static void LoadVSync()
-{
-    auto& cfg = ConfigSystem::Get()(u8"Display"sv);
-    if (cfg.at("VSync").get<bool>())
-    {
-        if (cfg.at("VSync_Adaptive").get<bool>())
-        {
-            if (SDL_GL_SetSwapInterval(-1) == 0)
-            {
-                OE_LOG(kWindow, kInfo, u8"Adaptive sync enabled"sv);
-            }
-            else
-            {
-                OE_LOG(kWindow, kWarn, u8"Adaptive sync not supported. Falling back to normal VSync..."sv);
-                SDL_GL_SetSwapInterval(1);
-            }
-        }
-        else
-        {
-            OE_LOG(kWindow, kInfo, u8"VSync enabled"sv);
-            SDL_GL_SetSwapInterval(1);
-        }
-    }
-    else
-    {
-        OE_LOG(kWindow, kInfo, u8"VSync disabled"sv);
-        SDL_GL_SetSwapInterval(0);
-    }
-}
-
 void SDLWindowDeleter::operator()(SDL_Window* window) const noexcept
 {
     SDL_DestroyWindow(window);
