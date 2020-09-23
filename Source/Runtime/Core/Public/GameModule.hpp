2de1da82a503d3a4e311bfd3872d1bcc1fabe6d4
Othereum <seokjin.dev@gmail.com>
2020-09-23 10:21:50 +0900

    refactor: Remove GAME_API

--- Source/Runtime/Core/Public/GameModule.hpp
+++ Source/Runtime/Core/Public/GameModule.hpp
@@ -2,6 +2,5 @@
 #include "Core.hpp"
 #include <string_view>
 
-#define GAME_API extern "C" OE_EXPORT
-#define DEFINE_GAME_MODULE(game_name) GAME_API const std::u8string_view kGameName = u8 ## #game_name ## sv
+#define DEFINE_GAME_MODULE(game_name) extern "C" OE_EXPORT const std::u8string_view kGameName = u8## #game_name##sv
 
