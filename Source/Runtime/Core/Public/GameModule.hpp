51f68d097e5817e2afe4f838380fb4cdf52eb48f
Othereum <seokjin.dev@gmail.com>
2020-09-23 09:29:45 +0900

    fix: constinit not supported yet

--- Source/Runtime/Core/Public/GameModule.hpp
+++ Source/Runtime/Core/Public/GameModule.hpp
@@ -3,5 +3,5 @@
 #include <string_view>
 
 #define GAME_API extern "C" OE_EXPORT
-#define DEFINE_GAME_MODULE(game_name) GAME_API constinit const std::u8string_view kGameName = u8 ## #game_name ## sv
+#define DEFINE_GAME_MODULE(game_name) GAME_API const std::u8string_view kGameName = u8 ## #game_name ## sv
 
